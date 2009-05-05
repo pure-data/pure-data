73df1ff74e3fa14b624f8144d6a14f0dab86ddbd
Miller Puckette <msp@ucsd.edu>
2009-05-05 11:22:00 -0700

    version number update

--- src/m_pd.h
+++ src/m_pd.h
@@ -10,8 +10,8 @@ extern "C" {
 
 #define PD_MAJOR_VERSION 0
 #define PD_MINOR_VERSION 42
-#define PD_BUGFIX_VERSION 4
-#define PD_TEST_VERSION ""
+#define PD_BUGFIX_VERSION 5
+#define PD_TEST_VERSION "test1"
 
 /* old name for "MSW" flag -- we have to take it for the sake of many old
 "nmakefiles" for externs, which will define NT and not MSW */
