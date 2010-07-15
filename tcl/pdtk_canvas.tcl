e025cb13666220f19c679e44211a057d36d540d1
Miller Puckette <msp@ucsd.edu>
2010-07-15 14:44:39 -0700

    fix bug opening files from menu

--- tcl/pdtk_canvas.tcl
+++ tcl/pdtk_canvas.tcl
@@ -208,13 +208,13 @@ proc ::pdtk_canvas::pdtk_canvas_popup {mytoplevel xcanvas ycanvas hasproperties 
 
 proc ::pdtk_canvas::started_loading_file {patchname} {
     # set the mouse cursor to look busy and grab focus so it stays that way    
-    .pdwindow.text -cursor watch
-    grab set -global .pdwindow.tcl.entry
+    .pdwindow.text configure -cursor watch
+    grab set .pdwindow.text
 }
 
 proc ::pdtk_canvas::finished_loading_file {mytoplevel} {
     .pdwindow.text configure -cursor xterm
-    grab release .pdwindow.tcl.entry
+    grab release .pdwindow.text
 
     # The grab seems to block the new PatchWindow from getting the FocusIn
     # event, so generate it after loading a patch.  Then it indeed is on top,
