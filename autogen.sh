7183cd959c29273d38159a8a5073cf82c6fedc1b
IOhannes m zmoelnig <zmoelnig@iem.at>
2010-07-16 23:25:46 +0200

    made automake system kind-of work
    this is a little addendum to make miller's inclusion of the automake-based build
    system actually work.
    
    autogen.sh: now creates ./m4 directory if necessary
    Makefile.am: include tcl
    tcl/Makefile: installs tcl-files into .../pd/tcl/
    po/Makefile: fixed the paths (tcl-files no longer in ./src)
    src/Makefile: removed tcl-files (no longer here)
    extra/expr~/makefile: fixed typo in the clean target
    extra/makefile: added targets to be called directly from ./extra
    
    handcrafted (non-automake) Makefiles now have supprt for rudimentary standard
    targets, like "install".
    
    Signed-off-by: msp <msp@fuzz.(none)>

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,13 @@
 
 PATH=/sw/bin:$PATH
 
+PWD=${0%/*}
+
+
+## git cannot really handle empty directories
+## so let's create the missing ones
+mkdir -p ${PWD}/m4
+
 case `uname -s` in
     MINGW*)
 # autoreconf doesn't always work on MinGW
