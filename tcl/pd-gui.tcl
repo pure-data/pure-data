zrnsm/pd-extended

added logpost(), pd findinstance, and buffer to dynamically filter log contents

tcl/pd-gui.tcl
@@ -106,7 +106,7 @@ set TCL_BUGFIX_VERSION 0
 set windowingsystem ""
 
 # args about how much and where to log
-set verbose $::pdwindow::maxverbosity
+set loglevel 2
 set stderr 0
 
 # connection between 'pd' and 'pd-gui'
@@ -441,14 +441,14 @@ proc find_default_font {} {
             break
         }
     }
-    ::pdwindow::verbose 1 "DEFAULT FONT: $::font_family"
+    ::pdwindow::verbose 0 "Default font: $::font_family\n"
 }
 
 proc set_base_font {family weight} {
     if {[lsearch -exact [font families] $family] > -1} {
         set ::font_family $family
     } else {
-        ::pdwindow::warn [format \
+        ::pdwindow::post [format \
             [_ "WARNING: Font family '%s' not found, using default (%s)\n"] \
                 $family $::font_family]
     }
@@ -456,7 +456,7 @@ proc set_base_font {family weight} {
         set ::font_weight $weight
         set using_defaults 0
     } else {
-        ::pdwindow::warn [format \
+        ::pdwindow::post [format \
             [_ "WARNING: Font weight '%s' not found, using default (%s)\n"] \
                 $weight $::font_weight]
     }
@@ -486,7 +486,7 @@ proc fit_font_into_metrics {} {
             "$::font_measured_metrics  $size\
                 [font measure $myfont M] [font metrics $myfont -linespace]"
         if {$giveup} {
-            ::pdwindow::warn [format \
+            ::pdwindow::post [format \
     [_ "WARNING: %s failed to find font size (%s) that fits into %sx%s!\n"]\
                [lindex [info level 0] 0] $size $width $height]
             continue
@@ -544,7 +544,6 @@ proc pdtk_check {mytoplevel message reply_to_pd default} {
 proc parse_args {argc argv} {
     opt_parser::init {
         {-stderr    set {::stderr}}
-        {-verbose   set {::verbose}}
         {-open      lappend {- ::filestoopen_list}}
     }
     set unflagged_files [opt_parser::get_options $argv]
@@ -653,7 +652,7 @@ proc check_for_running_instances {argc argv} {
 proc load_plugin_script {filename} {
     global errorInfo
 
-    ::pdwindow::verbose 1 "Loading plugin: $filename\n"
+    ::pdwindow::info "Loading plugin: $filename\n"
     set tclfile [open $filename]
     set tclcode [read $tclfile]
     close $tclfile
@@ -704,7 +703,7 @@ proc main {argc argv} {
             set ::fileopendir $::env(HOME)
         }
     }
-    ::pdwindow::verbose 1 "------------------ done with main ----------------------"
+    ::pdwindow::verbose 0 "------------------ done with main ----------------------\n"
 }
 
 main $::argc $::argv

tcl/pd_connect.tcl
@@ -19,7 +19,7 @@ proc ::pd_connect::configure_socket {sock} {
 # if pd opens first, it starts pd-gui, then pd-gui connects to the port pd sent
 proc ::pd_connect::to_pd {port {host localhost}} {
     variable pd_socket
-    ::pdwindow::verbose 5 "'pd-gui' connecting to 'pd' on localhost $port ..."
+    ::pdwindow::info "'pd-gui' connecting to 'pd' on localhost $port ...\n"
     if {[catch {set pd_socket [socket $host $port]}]} {
         puts stderr "WARNING: connect to pd failed, retrying port $host:$port."
         after 1000 ::pd_connect::to_pd $port $host
@@ -40,7 +40,7 @@ proc ::pd_connect::create_socket {} {
 
 proc ::pd_connect::from_pd {channel clientaddr clientport} {
     variable pd_socket $channel
-    ::pdwindow::verbose 5 "Connection from 'pd' to 'pd-gui' on $clientaddr:$clientport"
+    ::pdwindow::info "Connection from 'pd' to 'pd-gui' on $clientaddr:$clientport\n"
     ::pd_connect::configure_socket $pd_socket
 }
 
@@ -71,9 +71,9 @@ proc ::pd_connect::pd_readsocket {cmd_from_pd} {
             "missing close-brace" {
                 pd_readsocket $cmd_from_pd
             } "^invalid command name" {
-                ::pdwindow::fatal [concat [_ "(Tcl) INVALID COMMAND NAME: "] $errorInfo]
+                ::pdwindow::fatal [concat [_ "(Tcl) INVALID COMMAND NAME: "] $errorInfo "\n"]
             } default {
-                ::pdwindow::fatal [concat [_ "(Tcl) UNHANDLED ERROR: "] $errorInfo]
+                ::pdwindow::fatal [concat [_ "(Tcl) UNHANDLED ERROR: "] $errorInfo "\n"]
             }
         }
     }

pure-data/pure-data

tcl/pd-gui.tcl
@@ -106,7 +106,7 @@ set TCL_BUGFIX_VERSION 0
 set windowingsystem ""
 
 # args about how much and where to log
-set verbose $::pdwindow::maxverbosity
+set loglevel 2
 set stderr 0
 
 # connection between 'pd' and 'pd-gui'
@@ -423,14 +423,14 @@ proc find_default_font {} {
             break
         }
     }
-    ::pdwindow::verbose 1 "DEFAULT FONT: $::font_family"
+    ::pdwindow::verbose 0 "Default font: $::font_family\n"
 }
 
 proc set_base_font {family weight} {
     if {[lsearch -exact [font families] $family] > -1} {
         set ::font_family $family
     } else {
-        ::pdwindow::warn [format \
+        ::pdwindow::post [format \
             [_ "WARNING: Font family '%s' not found, using default (%s)\n"] \
                 $family $::font_family]
     }
@@ -438,7 +438,7 @@ proc set_base_font {family weight} {
         set ::font_weight $weight
         set using_defaults 0
     } else {
-        ::pdwindow::warn [format \
+        ::pdwindow::post [format \
             [_ "WARNING: Font weight '%s' not found, using default (%s)\n"] \
                 $weight $::font_weight]
     }
@@ -468,7 +468,7 @@ proc fit_font_into_metrics {} {
             "$::font_measured_metrics  $size\
                 [font measure $myfont M] [font metrics $myfont -linespace]"
         if {$giveup} {
-            ::pdwindow::warn [format \
+            ::pdwindow::post [format \
     [_ "WARNING: %s failed to find font size (%s) that fits into %sx%s!\n"]\
                [lindex [info level 0] 0] $size $width $height]
             continue
@@ -526,7 +526,6 @@ proc pdtk_check {mytoplevel message reply_to_pd default} {
 proc parse_args {argc argv} {
     opt_parser::init {
         {-stderr    set {::stderr}}
-        {-verbose   set {::verbose}}
         {-open      lappend {- ::filestoopen_list}}
     }
     set unflagged_files [opt_parser::get_options $argv]
@@ -635,7 +634,7 @@ proc check_for_running_instances {argc argv} {
 proc load_plugin_script {filename} {
     global errorInfo
 
-    ::pdwindow::verbose 1 "Loading plugin: $filename\n"
+    ::pdwindow::debug "Loading plugin: $filename\n"
     set tclfile [open $filename]
     set tclcode [read $tclfile]
     close $tclfile
@@ -686,7 +685,7 @@ proc main {argc argv} {
             set ::fileopendir $::env(HOME)
         }
     }
-    ::pdwindow::verbose 1 "------------------ done with main ----------------------"
+    ::pdwindow::verbose 0 "------------------ done with main ----------------------\n"
 }
 
 main $::argc $::argv

tcl/pd_connect.tcl
@@ -19,7 +19,7 @@ proc ::pd_connect::configure_socket {sock} {
 # if pd opens first, it starts pd-gui, then pd-gui connects to the port pd sent
 proc ::pd_connect::to_pd {port {host localhost}} {
     variable pd_socket
-    ::pdwindow::verbose 5 "'pd-gui' connecting to 'pd' on localhost $port ..."
+    ::pdwindow::debug "'pd-gui' connecting to 'pd' on localhost $port ...\n"
     if {[catch {set pd_socket [socket $host $port]}]} {
         puts stderr "WARNING: connect to pd failed, retrying port $host:$port."
         after 1000 ::pd_connect::to_pd $port $host
@@ -40,7 +40,7 @@ proc ::pd_connect::create_socket {} {
 
 proc ::pd_connect::from_pd {channel clientaddr clientport} {
     variable pd_socket $channel
-    ::pdwindow::verbose 5 "Connection from 'pd' to 'pd-gui' on $clientaddr:$clientport"
+    ::pdwindow::debug "Connection from 'pd' to 'pd-gui' on $clientaddr:$clientport\n"
     ::pd_connect::configure_socket $pd_socket
 }
 
@@ -79,9 +79,9 @@ proc ::pd_connect::pd_readsocket {cmd_from_pd} {
             "missing close-brace" {
                 pd_readsocket $cmd_from_pd
             } "^invalid command name" {
-                ::pdwindow::fatal [concat [_ "(Tcl) INVALID COMMAND NAME: "] $errorInfo]
+                ::pdwindow::fatal [concat [_ "(Tcl) INVALID COMMAND NAME: "] $errorInfo "\n"]
             } default {
-                ::pdwindow::fatal [concat [_ "(Tcl) UNHANDLED ERROR: "] $errorInfo]
+                ::pdwindow::fatal [concat [_ "(Tcl) UNHANDLED ERROR: "] $errorInfo "\n"]
             }
         }
     }