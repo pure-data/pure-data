3360aba7adb2f0fbc8a03a6a09757c60df279086
Hans-Christoph Steiner <hans@eds.org>
2011-03-10 23:40:00 -0500

    fix launching on Mac OS X

--- src/s_inter.c
+++ src/s_inter.c
@@ -1061,7 +1061,8 @@ int sys_startgui(const char *libdir)
                 if (stat(wish_paths[i], &statbuf) >= 0)
                     break;
             }
-            sprintf(cmdbuf,"\"%s\" %d\n", wish_paths[i], portno);
+            sprintf(cmdbuf,"\"%s\" \"%s/%spd-gui.tcl\" %d\n", 
+                    wish_paths[i], libdir, PDGUIDIR, portno);
 #else /* __APPLE__ */
             sprintf(cmdbuf,
   "TCL_LIBRARY=\"%s/lib/tcl/library\" TK_LIBRARY=\"%s/lib/tk/library\" \
