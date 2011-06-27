1b28fa6d02964881b42bda52e110dc64d2b8329c
Hans-Christoph Steiner <hans@eds.org>
2011-06-26 23:08:33 -0400

    removed bizarre 'k' character in 'package' command, causing Windows to not start

--- tcl/pd_guiprefs.tcl
+++ tcl/pd_guiprefs.tcl
@@ -127,7 +127,7 @@ proc get_config_aqua {adomain {akey} {arr false}} {
 # win: read in the registry
 #
 proc get_config_win {adomain {akey} {arr false}} {
-    pacḱage require registry
+    package require registry
     if {![catch {registry get $adomain $akey} conf]} {
         return [expr {$conf}]
     } {
@@ -177,7 +177,7 @@ proc write_config_aqua {data {adomain} {akey} {arr false}} {
 # if $arr is true, we write an array
 #
 proc write_config_win {data {adomain} {akey} {arr false}} {
-    pacḱage require registry
+    package require registry
     # FIXME: ugly
     if {$arr} {
         if {[catch {registry set $adomain $akey $data multi_sz} errorMsg]} {
