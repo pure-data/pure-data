354d575ba5625e99f23b6b7b746f6e787712a7bc
Marvin Humphrey <marvin@rectangular.com>
2011-10-09 21:03:55 -0700

    Remove unused UTF-8 routines.
    When the s_utf8 module was added to Pd, Bryan Jurish edited out some of the
    unused routines, but left in others that might potentially be used.  This
    patch removes all the unused functions and related variables.

--- src/s_utf8.h
+++ src/s_utf8.h
@@ -43,17 +43,8 @@
 /* is c the start of a utf8 sequence? */
 #define isutf(c) (((c)&0xC0)!=0x80)
 
-/* convert UTF-8 data to wide character */
-int u8_toucs(u_int32_t *dest, int sz, char *src, int srcsz);
-
-/* the opposite conversion */
-int u8_toutf8(char *dest, int sz, u_int32_t *src, int srcsz);
-
 /* moo: get byte length of character number, or 0 if not supported */
 int u8_wc_nbytes(u_int32_t ch);
-
-/* moo: compute required storage for UTF-8 encoding of 's[0..n-1]' */
-int u8_wcs_nbytes(u_int32_t *ucs, int size);
 
 /* single character to UTF-8, no NUL termination */
 int u8_wc_toutf8(char *dest, u_int32_t ch);
@@ -67,22 +58,10 @@ int u8_offset(char *str, int charnum);
 /* byte offset to character number */
 int u8_charnum(char *s, int offset);
 
-/* return next character, updating an index variable */
-u_int32_t u8_nextchar(char *s, int *i);
-
 /* move to next character */
 void u8_inc(char *s, int *i);
 
 /* move to previous character */
 void u8_dec(char *s, int *i);
-
-/* moo: move pointer to next character */
-void u8_inc_ptr(char **sp);
-
-/* moo: move pointer to previous character */
-void u8_dec_ptr(char **sp);
-
-/* returns length of next utf-8 sequence */
-int u8_seqlen(char *s);
 
 #endif /* S_UTF8_H */
