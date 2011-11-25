a45dbbc20eb560eab3707e603a10a2e529f32d9a
Miller Puckette <msp@ucsd.edu>
2011-11-24 19:52:36 -0800

    try to fix crasher bug from test5 and clean up TK errors a different way

--- src/g_graph.c
+++ src/g_graph.c
@@ -95,8 +95,9 @@ void glist_delete(t_glist *x, t_gobj *y)
             }
             else
             {
-                text_eraseborder(&gl->gl_obj, x,
-                    rtext_gettag(glist_findrtext(x, &gl->gl_obj)));
+                if (glist_isvisible(x))
+                    text_eraseborder(&gl->gl_obj, x,
+                        rtext_gettag(glist_findrtext(x, &gl->gl_obj)));
             }
         }
     }
@@ -105,11 +106,9 @@ void glist_delete(t_glist *x, t_gobj *y)
     if (drawcommand)
         canvas_redrawallfortemplate(template_findbyname(canvas_makebindsym(
             glist_getcanvas(x)->gl_name)), 2);
+    gobj_delete(y, x);
     if (glist_isvisible(canvas))
-    {
-        gobj_delete(y, x);
         gobj_vis(y, x, 0);
-    }
     if (x->gl_editor && (ob = pd_checkobject(&y->g_pd)))
         rtext_new(x, ob);
     if (x->gl_list == y) x->gl_list = y->g_next;
