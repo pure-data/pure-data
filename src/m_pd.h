271d4a1d751b3047613d4fe036a1eded003cec2a
msp <msp@stripe.(none)>
2012-07-28 17:43:45 -0700

    oops, version number confusion in m_pd.h

--- src/m_pd.h
+++ src/m_pd.h
@@ -9,8 +9,8 @@ extern "C" {
 #endif
 
 #define PD_MAJOR_VERSION 0
-#define PD_MINOR_VERSION 43
-#define PD_BUGFIX_VERSION 4
+#define PD_MINOR_VERSION 44
+#define PD_BUGFIX_VERSION 0
 #define PD_TEST_VERSION "test0"
 
 /* old name for "MSW" flag -- we have to take it for the sake of many old
