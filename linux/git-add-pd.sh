d46abab76981bc3619757ea726d62679c42dcad9
msp <msp@stripe.(none)>
2012-08-31 16:53:40 -0700

    aha, that was why.

--- linux/git-add-pd.sh
+++ linux/git-add-pd.sh
@@ -1,5 +1,5 @@
 (cd ~/pd/src; sh ../linux/detab-src.sh)
 
 cd ~/pd
-git add INSTALL.txt LICENSE.txt README.txt doc extra man \
+git add INSTALL.txt LICENSE.txt README.txt configure.ac doc extra man \
     portaudio portmidi src tcl msw linux
