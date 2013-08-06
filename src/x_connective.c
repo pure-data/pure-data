c598889874b139eec6367b47e9fdb7d3a6d20b3c
Miller Puckette <msp@ucsd.edu>
2013-08-06 16:15:27 -0700

    Fix 'value foo' to catch "float" mesages to foo as a symbol (send of message box)

--- src/x_connective.c
+++ src/x_connective.c
@@ -1494,6 +1494,11 @@ value_setfloat(t_symbol *s, t_float f)
     return (0); 
 }
 
+static void vcommon_float(t_vcommon *x, t_float f)
+{
+    x->c_f = f;
+}
+
 static void *value_new(t_symbol *s)
 {
     t_value *x = (t_value *)pd_new(value_class);
@@ -1528,6 +1533,7 @@ static void value_setup(void)
     class_addfloat(value_class, value_float);
     vcommon_class = class_new(gensym("value"), 0, 0,
         sizeof(t_vcommon), CLASS_PD, 0);
+    class_addfloat(vcommon_class, vcommon_float);
 }
 
 /* -------------- overall setup routine for this file ----------------- */
