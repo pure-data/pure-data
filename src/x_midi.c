d0953882aec18abc977c0586fc07752f778af936
Miller Puckette <msp@ucsd.edu>
2013-08-22 12:20:17 -0700

    suppress "midirealtimein: works under MSW only" message
    standard-out-garbage workaround improved for OSX

--- src/x_midi.c
+++ src/x_midi.c
@@ -612,9 +612,6 @@ static void *midirealtimein_new( void)
     x->x_outlet1 = outlet_new(&x->x_obj, &s_float);
     x->x_outlet2 = outlet_new(&x->x_obj, &s_float);
     pd_bind(&x->x_obj.ob_pd, midirealtimein_sym);
-#ifndef _WIN32
-    pd_error(x, "midirealtimein: works under MSW only");
-#endif
     return (x);
 }
 
