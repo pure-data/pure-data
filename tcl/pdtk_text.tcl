14cc59fab7d6bd6dcc798fe745b020d50cea1105
Miller Puckette <msp@ucsd.edu>
2013-09-21 19:51:26 -0700

    Try again to fix text paste

--- tcl/pdtk_text.tcl
+++ tcl/pdtk_text.tcl
@@ -34,7 +34,7 @@ proc pdtk_pastetext {tkcanvas} {
         for {set i 0} {$i < [string length $pdtk_pastebuffer]} {incr i 1} {
             set cha [string index $pdtk_pastebuffer $i]
             scan $cha %c keynum
-            pdsend "$tkcanvas key 1 $keynum 0"
+            pdsend "[winfo toplevel $tkcanvas] key 1 $keynum 0"
         }
     }
 }
