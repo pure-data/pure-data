c410552de68cdb6768c10bf2af333ac9caea51e5
Chris McCormick <chris@mccormick.cx>
2015-05-24 16:38:15 +0800

    Passing arguments through as a list of lists. Sorting results by date and reversing to get latest uploaded first.

--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -63,30 +63,40 @@ proc ::dialog_externals_search::create_dialog {mytoplevel} {
 }
 
 proc ::dialog_externals_search::initiate_search {mytoplevel} {
+    # let the user know what we're doing
+    $mytoplevel.results delete 1.0 end
+    $mytoplevel.results insert end "Searching for externals...\n"
+    # make the ajax call
     set results [search_for [$mytoplevel.entry get]]
-    # build the list UI of results
+    # sort the results by reverse date - latest upload first
+    set sorted [lsort -index 3 $results]
+    for {set i [llength $sorted]} {[incr i -1] >= 0} {} {lappend reversed [lindex $sorted $i]}
     # delete all text in the results
     $mytoplevel.results delete 1.0 end
     set counter 0
-    foreach {title URL creator date} $results {
+    # build the list UI of results
+    foreach r $reversed {
         # $current_listbox insert end "$creator - $date\n\t$title\n\t$URL"
+        foreach {title URL creator date} $r {break}
         set tag ch$counter
-        $mytoplevel.results insert end "$title\n\tBy $creator - $date\n\t$URL\n\n" $tag
+        $mytoplevel.results insert end "$title\n\tBy $creator - $date\n\n" $tag
         $mytoplevel.results tag bind $tag <Enter> "$mytoplevel.results tag configure $tag -foreground blue"
         $mytoplevel.results tag bind $tag <Leave> "$mytoplevel.results tag configure $tag -foreground black"
-        $mytoplevel.results tag bind $tag <1> [list "dialog_externals_search::clicked_link" .t $tag $counter $URL]
+        $mytoplevel.results tag bind $tag <1> [list "dialog_externals_search::clicked_link" $mytoplevel $URL $title]
         incr counter
     }
 }
 
 # handle a clicked link
-proc ::dialog_externals_search::clicked_link {t tag counter URL} {
-    ::pdwindow::post "$t $tag $counter $URL"
+proc ::dialog_externals_search::clicked_link {mytoplevel URL title} {
+    $mytoplevel.results delete 1.0 end
+    $mytoplevel.results insert end "Unzip $title\n"
+    $mytoplevel.results insert end "Copy the files into $::current_plugin_loadpath.\n"
+    $mytoplevel.results insert end "Downloading:\n$URL\nInto $::current_plugin_loadpath...\n"
 }
 
 # make a remote HTTP call and parse and display the results
 proc ::dialog_externals_search::search_for {term} {
-    ::pdwindow::post "Searching for externals...\n"
     set searchresults [list]
     set token [http::geturl "http://puredata.info/search_rss?SearchableText=$term+xtrnl+.zip&portal_type%3Alist=IAEMFile"]
     set contents [http::data $token]
@@ -94,11 +104,11 @@ proc ::dialog_externals_search::search_for {term} {
     # loop through the resulting XML parsing out entries containing results with a regular expression
     foreach ele $splitCont {
         if {[regexp {<title>(.*?)</title>(.*?)<link>(.*?)</link>(.*?)<dc:creator>(.*?)</dc:creator>(.*?)<dc:date>(.*?)</dc:date>} $ele -> title junk URL junk creator junk date]} {
-            lappend searchresults $title $URL $creator $date
+            set result [list $title $URL $creator $date]
+            lappend searchresults $result
         }
     }
     http::cleanup $token
-    ::pdwindow::post "Done.\n"
     return $searchresults
 }
 
