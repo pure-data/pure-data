fc6a531b83ddb33373cc15a1728b4a67deea21ed
Chris McCormick <chris@mccormick.cx>
2015-05-24 20:31:50 +0800

    Downloading and opening zipfile from the found-list and the destination folder now working correctly.

--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -13,8 +13,9 @@ package require Tcl 8.4
 # + require everything and all your script needs.
 #   If a requirement is missing,
 #   Pd will load, but the script will not.
-package require pdwindow 0.1
 package require http 2
+package require pdwindow 0.1
+package require pd_menucommands 0.1
 
 # console message to let them know we're loaded
 pdwindow::post  "deken-plugin.tcl (Pd externals search) in $::current_plugin_loadpath loaded.\n"
@@ -23,6 +24,7 @@ pdwindow::post "Platform detected: $tcl_platform(os)-$tcl_platform(machine)-$tcl
 
 namespace eval ::dialog_externals_search:: {
     namespace export open_searchui
+    variable mytoplevelref
 }
 
 # this function gets called when the menu is clicked
@@ -39,8 +41,9 @@ proc ::dialog_externals_search::open_searchui {mytoplevel} {
 # build the externals search dialog window
 proc ::dialog_externals_search::create_dialog {mytoplevel} {
     toplevel $mytoplevel -class DialogWindow
+    variable mytoplevelref $mytoplevel
     wm title $mytoplevel [_ "Find externals"]
-    wm geometry $mytoplevel 670x550+0+30
+    wm geometry $mytoplevel 670x550
     wm minsize $mytoplevel 230 360
     wm transient $mytoplevel
     $mytoplevel configure -padx 10 -pady 5
@@ -61,7 +64,6 @@ proc ::dialog_externals_search::create_dialog {mytoplevel} {
 
     text $mytoplevel.results -takefocus 0
     pack $mytoplevel.results -side top -padx 6 -pady 3 -fill x
-
 }
 
 proc ::dialog_externals_search::initiate_search {mytoplevel} {
@@ -77,24 +79,55 @@ proc ::dialog_externals_search::initiate_search {mytoplevel} {
     $mytoplevel.results delete 1.0 end
     set counter 0
     # build the list UI of results
+    # TODO: filter these for the platform, machine, and bits
     foreach r $reversed {
-        # $current_listbox insert end "$creator - $date\n\t$title\n\t$URL"
         foreach {title URL creator date} $r {break}
         set tag ch$counter
         $mytoplevel.results insert end "$title\n\tBy $creator - $date\n\n" $tag
         $mytoplevel.results tag bind $tag <Enter> "$mytoplevel.results tag configure $tag -foreground blue"
         $mytoplevel.results tag bind $tag <Leave> "$mytoplevel.results tag configure $tag -foreground black"
-        $mytoplevel.results tag bind $tag <1> [list "dialog_externals_search::clicked_link" $mytoplevel $URL $title]
+        # have to decode the URL here because otherwise percent signs cause tcl to bug out - not sure why - scripting languages...
+        $mytoplevel.results tag bind $tag <1> [list dialog_externals_search::clicked_link $mytoplevel [urldecode $URL] $title]
         incr counter
     }
 }
 
 # handle a clicked link
 proc ::dialog_externals_search::clicked_link {mytoplevel URL title} {
+    set destination "$::current_plugin_loadpath/$title"
     $mytoplevel.results delete 1.0 end
-    $mytoplevel.results insert end "Unzip $title\n"
-    $mytoplevel.results insert end "Copy the files into $::current_plugin_loadpath.\n"
-    $mytoplevel.results insert end "Downloading:\n$URL\nInto $::current_plugin_loadpath...\n"
+    $mytoplevel.results insert end "Commencing downloading of:\n$URL\nInto $::current_plugin_loadpath...\n"
+    dialog_externals_search::download_file $URL $destination
+    # Open both the destination folder and the zipfile itself
+    # NOTE: in tcl 8.5 it should be possible to use the zlib interface to actually do the unzip
+    pd_menucommands::menu_openfile $::current_plugin_loadpath
+    pd_menucommands::menu_openfile $destination
+    # destroy $mytoplevel
+    $mytoplevel.results insert end "1. Unzip $destination.\n"
+    $mytoplevel.results insert end "2. Copy the files into $::current_plugin_loadpath.\n\n"
+}
+
+# download a file to a location
+# http://wiki.tcl.tk/15303
+proc ::dialog_externals_search::download_file {URL outputfilename} {
+    set f [open $outputfilename w]
+    set status ""
+    set errorstatus ""
+    fconfigure $f -translation binary
+    set httpresult [http::geturl $URL -binary true -progress "dialog_externals_search::download_progress" -channel $f]
+    set status [::http::status $httpresult]
+    set errorstatus [::http::error $httpresult]
+    flush $f
+    close $f
+    http::cleanup $httpresult
+    return status errorstatus
+}
+
+# print the download progress to the results window
+proc ::dialog_externals_search::download_progress {token total current} {
+    variable mytoplevelref
+    set computed [expr {round(100 * (1.0 * $current / $total))}]
+    $mytoplevelref.results insert end "= $computed%\n"
 }
 
 # make a remote HTTP call and parse and display the results
@@ -125,3 +158,11 @@ $mymenu insert $inserthere separator
 $mymenu insert $inserthere command -label [_ "Find externals"] -command {::dialog_externals_search::open_searchui .externals_searchui}
 # bind all <$::modifier-Key-s> {::dialog_externals_search::open_helpbrowser .helpbrowser2}
 
+# http://rosettacode.org/wiki/URL_decoding#Tcl
+proc urldecode {str} {
+    set specialMap {"[" "%5B" "]" "%5D"}
+    set seqRE {%([0-9a-fA-F]{2})}
+    set replacement {[format "%c" [scan "\1" "%2x"]]}
+    set modStr [regsub -all $seqRE [string map $specialMap $str] $replacement]
+    return [encoding convertfrom utf-8 [subst -nobackslash -novariable $modStr]]
+}
