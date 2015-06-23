474c1a010a4ef9b25158bdf2e64c5cb2cd80de3c
Roman Haefeli <reduzent@gmail.com>
2015-06-23 15:10:36 +0200

    fix windows statis path order

--- src/s_path.c
+++ src/s_path.c
@@ -252,9 +252,9 @@ void sys_setextrapath(const char *p)
 #endif
 
 #ifdef _WIN32
-    sys_expandpath("%CommonProgramFiles%/Pd", pathbuf, MAXPDSTRING);
-    sys_staticpath = namelist_append(0, pathbuf, 0);
     sys_expandpath("%AppData%/Pd", pathbuf, MAXPDSTRING);
+    sys_staticpath = namelist_append(0, pathbuf, 0);
+    sys_expandpath("%CommonProgramFiles%/Pd", pathbuf, MAXPDSTRING);
     sys_staticpath = namelist_append(sys_staticpath, pathbuf, 0);
 #endif
     /* add built-in "extra" path last so its checked last */
