bbb0b68282280c4d5f105600a61f1bddad90b18b
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-07-01 22:52:06 +0200

    apt-backend for deken
    it's only enabled if we detect that the system is running a Debian derivative.
    
    Closes https://github.com/pure-data/deken/issues/13
    
    TODO: during package installation, the GUI freezes :-(

--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -418,6 +418,7 @@ proc urldecode {str} {
 
 
 
+## ####################################################################
 ## searching puredata.info
 proc ::deken::search::puredata.info {term} {
     set searchresults [list]
@@ -450,3 +451,84 @@ proc ::deken::search::puredata.info {term} {
     return [lsort -dictionary -decreasing -index 5 $searchresults ]
 }
 ::deken::register ::deken::search::puredata.info
+
+
+## ####################################################################
+## searching apt (if available)
+namespace eval ::deken::apt {
+    namespace export search
+    namespace export install
+    variable distribution
+}
+
+if { [ catch { exec lsb_release -si } ::deken::apt::distribution options ] } { unset ::deken::apt::distribution }
+
+proc ::deken::apt::search {name} {
+    set result []
+    if { [ catch { exec apt-cache madison } _ options ] } {
+        ::deken::post "Unable to run 'apt-cache madison'" error
+    } {
+    set name [ string tolower $name ]
+    if { "$name" eq "gem" } {
+        set searchname $name
+    } elseif { "$name" eq "" } {
+        set searchname "^pd-.*"
+    } else {
+        set searchname "^pd-${name}.*"
+    }
+    array unset pkgs
+    array set pkgs {}
+    set io [ open "|apt-cache madison $searchname" r ]
+    while { [ gets $io line ] >= 0 } {
+        #puts $line
+        set llin [ split "$line" "|" ]
+        set pkgname [ string trim [ lindex $llin 0 ] ]
+
+        #if { $pkgname ne $searchname } { continue }
+        set ver_  [ string trim [ lindex $llin 1 ] ]
+        set info_ [ string trim [ lindex $llin 2 ] ]
+        if { "Packages" eq [ lindex $info_ end ] } {
+            set suite [ lindex $info_ 1 ]
+            set arch  [ lindex $info_ 2 ]
+            if { ! [ info exists pkgs($ver_) ] } {
+                set pkgs($ver_) [ list $pkgname $suite $arch ]
+            }
+        }
+    }
+    foreach {v inf} [ array get pkgs ] {
+        set pkgname [ lindex $inf 0 ]
+        set suite   [ lindex $inf 1 ]
+        set arch    [ lindex $inf 2 ]
+        set name $pkgname/$v
+        set cmd "::deken::apt::install ${pkgname}=$v"
+        set match 1
+        set comment "Provided by ${::deken::apt::distribution} (${suite})"
+        set status "${pkgname}_${v}_${arch}.deb"
+
+        lappend result [list $name $cmd $match $comment $status]
+    }
+    }
+    return [lsort -dictionary -decreasing -index 1 $result ]
+}
+
+proc ::deken::apt::install {pkg} {
+    if { [ catch { exec which gksudo } gsudo options ] } {
+        ::deken::post "Please install 'gksudo', if you want to install system packages via deken..." error
+    } {
+        set prog "apt-get install -y --show-progress ${pkg}"
+        # for whatever reasons, we cannot have 'deken' as the description
+        # (it will always show $prog instead)
+        set desc deken::apt
+        set cmdline "$gsudo -D $desc -- $prog"
+        #::deken::post $cmdline
+        set io [ open "|${cmdline}" ]
+        while { [ gets $io line ] >= 0 } {
+            ::deken::post "apt: $line"
+        }
+    }
+}
+
+# only add this backend, if we are actually running Debian or a derivative
+if { [ info exists ::deken::apt::distribution ] } {
+  ::deken::register ::deken::apt::search
+}
