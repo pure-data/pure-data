e61d59a8d043e1ec20953047dd73c456cc06b7b4
danomatika <danomatika@gmail.com>
2016-03-15 04:35:03 -0600

    dialog_path & dialog_startup: added horz padding to buttons

--- tcl/scrollbox.tcl
+++ tcl/scrollbox.tcl
@@ -181,9 +181,9 @@ proc ::scrollbox::make { mytoplevel listdata add_method edit_method } {
     button $mytoplevel.actions.delete_path -text {Delete} \
         -command "::scrollbox::delete_item $mytoplevel"
 
-    pack $mytoplevel.actions.delete_path -side right -pady 2m
-    pack $mytoplevel.actions.edit_path -side right -pady 2m
-    pack $mytoplevel.actions.add_path -side right -pady 2m
+    pack $mytoplevel.actions.delete_path -side right -pady 2m -padx 5
+    pack $mytoplevel.actions.edit_path -side right -pady 2m -padx 5
+    pack $mytoplevel.actions.add_path -side right -pady 2m -padx 5
 
     $mytoplevel.listbox.box activate end
     $mytoplevel.listbox.box selection set end
--- tcl/scrollboxwindow.tcl
+++ tcl/scrollboxwindow.tcl
@@ -84,13 +84,13 @@ proc ::scrollboxwindow::make {mytoplevel listdata add_method edit_method commit_
 
     button $mytoplevel.nb.buttonframe.cancel -text [_ "Cancel"] \
         -command "::scrollboxwindow::cancel $mytoplevel"
-    pack $mytoplevel.nb.buttonframe.cancel -side left -expand 1 -fill x
+    pack $mytoplevel.nb.buttonframe.cancel -side left -expand 1 -fill x -padx 5
     if {$::windowingsystem ne "aqua"} {
         button $mytoplevel.nb.buttonframe.apply -text [_ "Apply"] \
             -command "::scrollboxwindow::apply $mytoplevel $commit_method"
-        pack $mytoplevel.nb.buttonframe.apply -side left -expand 1 -fill x
+        pack $mytoplevel.nb.buttonframe.apply -side left -expand 1 -fill x -padx 5
     }
     button $mytoplevel.nb.buttonframe.ok -text [_ "OK"] \
         -command "::scrollboxwindow::ok $mytoplevel $commit_method"
-    pack $mytoplevel.nb.buttonframe.ok -side left -expand 1 -fill x
+    pack $mytoplevel.nb.buttonframe.ok -side left -expand 1 -fill x -padx 5
 }
