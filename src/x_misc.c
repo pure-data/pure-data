4a1d94ee9a0e168536b3c0c11b94c3e80851b828
Miller Puckette <msp@ucsd.edu>
2016-05-04 13:29:03 -0700

    doc updates suggested by Alexandre Porres - plus, took out the non-working "serial" object

--- src/x_misc.c
+++ src/x_misc.c
@@ -157,48 +157,6 @@ static void namecanvas_setup(void)
             sizeof(t_namecanvas), CLASS_NOINLET, A_DEFSYM, 0);
 }
 
-/* ---------------serial ports (_WIN32 only -- hack) ------------------------- */
-#define MAXSERIAL 100
-
-static t_class *serial_class;
-
-typedef struct _serial
-{
-    t_object x_obj;
-    int x_portno;
-    int x_open;
-} t_serial;
-
-static void serial_float(t_serial *x, t_float f)
-{
-    int n = f;
-    char message[MAXSERIAL * 4 + 100];
-    if (!x->x_open)
-    {
-        sys_vgui("com%d_open\n", x->x_portno);
-        x->x_open = 1;
-    }
-    sprintf(message, "com%d_send \"\\%3.3o\"\n", x->x_portno, n);
-    sys_gui(message);
-}
-
-static void *serial_new(t_floatarg fportno)
-{
-    int portno = fportno;
-    t_serial *x = (t_serial *)pd_new(serial_class);
-    if (!portno) portno = 1;
-    x->x_portno = portno;
-    x->x_open = 0;
-    return (x);
-}
-
-static void serial_setup(void)
-{
-    serial_class = class_new(gensym("serial"), (t_newmethod)serial_new, 0,
-        sizeof(t_serial), 0, A_DEFFLOAT, 0);
-    class_addfloat(serial_class, serial_float);
-}
-
 /* -------------------------- cputime ------------------------------ */
 
 static t_class *cputime_class;
@@ -727,7 +685,6 @@ void x_misc_setup(void)
     random_setup();
     loadbang_setup();
     namecanvas_setup();
-    serial_setup();
     cputime_setup();
     realtime_setup();
     oscparse_setup();
