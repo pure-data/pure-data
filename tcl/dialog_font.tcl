d11c0f75e37043a48de4602f912dc48be8882de3
danomatika <danomatika@gmail.com>
2016-05-22 18:21:04 -0600

    set Font dialog title

--- tcl/dialog_font.tcl
+++ tcl/dialog_font.tcl
@@ -97,6 +97,7 @@ proc ::dialog_font::create_dialog {gfxstub} {
     .font configure -menu $::dialog_menubar
     .font configure -padx 10 -pady 5
     wm group .font .
+    wm title .font "Font"
 #    wm resizable .font 0 0
     wm transient .font $::focused_window
     ::pd_bindings::dialog_bindings .font "font"
