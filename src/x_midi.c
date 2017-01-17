f92f6ae2c3bf6cb587ac585041b7fa729268c51f
Albert Graef <aggraef@gmail.com>
2017-01-17 22:43:01 +0100

    Fix up bendin so that it delivers values starting at -8192 to be consistent with bendout.

--- src/x_midi.c
+++ src/x_midi.c
@@ -356,7 +356,7 @@ void inmidi_pitchbend(int portno, int channel, int value)
     if (pd_this->pd_bendin_sym->s_thing)
     {
         t_atom at[2];
-        SETFLOAT(at, value);
+        SETFLOAT(at, value-8192);
         SETFLOAT(at+1, (channel + (portno << 4) + 1));
         pd_list(pd_this->pd_bendin_sym->s_thing, &s_list, 2, at);
     }
