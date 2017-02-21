07affde3e99c162185b58dac644cbe2b1e7d8dc8
IOhannes m zmölnig <zmoelnig@iem.at>
2017-02-21 13:46:48 +0100

    rather than manually dispatching the config get/set functions, define the procs differently on the various platforms
    $::windowingsystem is only defined when calling ::pd_guiprefs::init, so
    the os-specific implementations are defined within the init-function.
    
    we also provide stubs, so we get a nice error rather than failing silently,
    if the windowing system is unknown...

--- tcl/pd_guiprefs.tcl
+++ tcl/pd_guiprefs.tcl
@@ -25,9 +25,119 @@ set ::pd_guiprefs::domain ""
 #
 proc ::pd_guiprefs::init {} {
     switch -- $::windowingsystem {
-        "aqua"  { init_aqua }
-        "win32" { init_win }
-        "x11"   { init_x11 }
+        "aqua" {
+            init_aqua
+            # ------------------------------------------------------------------------------
+            # osx: read a plist file
+            #
+            proc ::pd_guiprefs::get_config {adomain {akey} {arr false}} {
+                if {![catch {exec defaults read $adomain $akey} conf]} {
+                    if {$arr} {
+                        set conf [plist_array_to_tcl_list $conf]
+                    }
+                } else {
+                    # initialize NSRecentDocuments with an empty array
+                    exec defaults write $adomain $akey -array
+                    set conf {}
+                }
+                return $conf
+            }
+            # ------------------------------------------------------------------------------
+            # write configs to plist file
+            # if $arr is true, we write an array
+            #
+            proc ::pd_guiprefs::write_config {data {adomain} {akey} {arr false}} {
+                # FIXME empty and write again so we don't loose the order
+                if {[catch {exec defaults write $adomain $akey -array} errorMsg]} {
+                    ::pdwindow::error "write_config $akey: $errorMsg"
+                }
+                if {$arr} {
+                    foreach filepath $data {
+                        set escaped [escape_for_plist $filepath]
+                        exec defaults write $adomain $akey -array-add "$escaped"
+                    }
+                } else {
+                    set escaped [escape_for_plist $data]
+                    exec defaults write $adomain $akey '$escaped'
+                }
+
+                # Disable window state saving by default for 10.7+ as there is a chance
+                # pd will hang on start due to conflicting patch resources until the state
+                # is purged. State saving will still work, it just has to be explicitly
+                # asked for by holding the Option/Alt button when quitting via the File
+                # menu or with the Cmd+Q key binding.
+                exec defaults write $adomain NSQuitAlwaysKeepsWindows -bool false
+            }
+
+        }
+        "win32" {
+            init_win
+            # ------------------------------------------------------------------------------
+            # w32: read in the registry
+            #
+            proc ::pd_guiprefs::get_config {adomain {akey} {arr false}} {
+                package require registry
+                if {![catch {registry get $adomain $akey} conf]} {
+                    return [expr {$conf}]
+                } else {
+                    return {}
+                }
+            }
+            # ------------------------------------------------------------------------------
+            # w32: write configs to registry
+            # if $arr is true, we write an array
+            #
+            proc ::pd_guiprefs::write_config {data {adomain} {akey} {arr false}} {
+                package require registry
+                # FIXME: ugly
+                if {$arr} {
+                    if {[catch {registry set $adomain $akey $data multi_sz} errorMsg]} {
+                        ::pdwindow::error "write_config $data $akey: $errorMsg"
+                    }
+                } else {
+                    if {[catch {registry set $adomain $akey $data sz} errorMsg]} {
+                        ::pdwindow::error "write_config $data $akey: $errorMsg"
+                    }
+                }
+            }
+
+        }
+        "x11" {
+            init_x11
+            # ------------------------------------------------------------------------------
+            # linux: read a config file and return its lines splitted.
+            #
+            proc ::pd_guiprefs::get_config {adomain {akey} {arr false}} {
+                set filename [file join $adomain $akey]
+                set conf {}
+                if {
+                    [file exists $filename] == 1
+                    && [file readable $filename]
+                } {
+                    set fl [open $filename r]
+                    while {[gets $fl line] >= 0} {
+                        lappend conf $line
+                    }
+                    close $fl
+                }
+                return $conf
+            }
+            # ------------------------------------------------------------------------------
+            # linux: write configs to USER_APP_CONFIG_DIR
+            # $arr is true if the data needs to be written in an array
+            #
+            proc ::pd_guiprefs::write_config {data {adomain} {akey} {arr false}} {
+                # right now I (yvan) assume that data are just \n separated, i.e. no keys
+                set data [join $data "\n"]
+                set filename [file join $adomain $akey]
+                if {[catch {set fl [open $filename w]} errorMsg]} {
+                    ::pdwindow::error "write_config $data $akey: $errorMsg"
+                } else {
+                    puts -nonewline $fl $data
+                    close $fl
+                }
+            }
+        }
     }
     # assign gui preferences
     # osx special case for arrays
@@ -81,139 +191,14 @@ proc ::pd_guiprefs::update_recentfiles {afile} {
 # main read/write procedures
 #################################################################
 
-# ------------------------------------------------------------------------------
-# get configs from a file or the registry
-#
-proc ::pd_guiprefs::get_config {adomain {akey} {arr}} {
-    switch -- $::windowingsystem {
-        "aqua"  { set conf [get_config_aqua $adomain $akey $arr] }
-        "win32" { set conf [get_config_win $adomain $akey $arr] }
-        "x11"   { set conf [get_config_x11 $adomain $akey $arr] }
-    }
-    return $conf
-}
-
-# ------------------------------------------------------------------------------
-# write configs to a file or to the registry
-# $arr is true if the data needs to be written in an array
-#
+## these are stubs that will be overwritten in ::pd_guiprefs::init()
 proc ::pd_guiprefs::write_config {data {adomain} {akey} {arr false}} {
-    switch -- $::windowingsystem {
-        "aqua"  { write_config_aqua $data $adomain $akey $arr }
-        "win32" { write_config_win $data $adomain $akey $arr }
-        "x11"   { write_config_x11 $data $adomain $akey }
-    }
+    ::pdwindow::error "::pd_guiprefs::write_config not implemented for $::windowingsystem"
+}
+proc ::pd_guiprefs::get_config {adomain {akey} {arr false}} {
+    ::pdwindow::error "::pd_guiprefs::get_config not implemented for $::windowingsystem"
 }
 
-#################################################################
-# os specific procedures
-#################################################################
-
-# ------------------------------------------------------------------------------
-# osx: read a plist file
-#
-proc ::pd_guiprefs::get_config_aqua {adomain {akey} {arr false}} {
-    if {![catch {exec defaults read $adomain $akey} conf]} {
-        if {$arr} {
-            set conf [plist_array_to_tcl_list $conf]
-        }
-    } else {
-        # initialize NSRecentDocuments with an empty array
-        exec defaults write $adomain $akey -array
-        set conf {}
-    }
-    return $conf
-}
-
-# ------------------------------------------------------------------------------
-# win: read in the registry
-#
-proc ::pd_guiprefs::get_config_win {adomain {akey} {arr false}} {
-    package require registry
-    if {![catch {registry get $adomain $akey} conf]} {
-        return [expr {$conf}]
-    } else {
-        return {}
-    }
-}
-
-# ------------------------------------------------------------------------------
-# linux: read a config file and return its lines splitted.
-#
-proc ::pd_guiprefs::get_config_x11 {adomain {akey} {arr false}} {
-    set filename [file join $adomain $akey]
-    set conf {}
-    if {
-        [file exists $filename] == 1
-        && [file readable $filename]
-    } {
-        set fl [open $filename r]
-        while {[gets $fl line] >= 0} {
-           lappend conf $line
-        }
-        close $fl
-    }
-    return $conf
-}
-
-# ------------------------------------------------------------------------------
-# osx: write configs to plist file
-# if $arr is true, we write an array
-#
-proc ::pd_guiprefs::write_config_aqua {data {adomain} {akey} {arr false}} {
-    # FIXME empty and write again so we don't loose the order
-    if {[catch {exec defaults write $adomain $akey -array} errorMsg]} {
-        ::pdwindow::error "write_config_aqua $akey: $errorMsg"
-    }
-    if {$arr} {
-        foreach filepath $data {
-            set escaped [escape_for_plist $filepath]
-            exec defaults write $adomain $akey -array-add "$escaped"
-        }
-    } else {
-        set escaped [escape_for_plist $data]
-        exec defaults write $adomain $akey '$escaped'
-    }
-
-    # Disable window state saving by default for 10.7+ as there is a chance
-    # pd will hang on start due to conflicting patch resources until the state
-    # is purged. State saving will still work, it just has to be explicitly
-    # asked for by holding the Option/Alt button when quitting via the File
-    # menu or with the Cmd+Q key binding.
-    exec defaults write $adomain NSQuitAlwaysKeepsWindows -bool false
-}
-
-# ------------------------------------------------------------------------------
-# win: write configs to registry
-# if $arr is true, we write an array
-#
-proc ::pd_guiprefs::write_config_win {data {adomain} {akey} {arr false}} {
-    package require registry
-    # FIXME: ugly
-    if {$arr} {
-        if {[catch {registry set $adomain $akey $data multi_sz} errorMsg]} {
-            ::pdwindow::error "write_config_win $data $akey: $errorMsg"
-        }
-    } else {
-        if {[catch {registry set $adomain $akey $data sz} errorMsg]} {
-            ::pdwindow::error "write_config_win $data $akey: $errorMsg"
-        }
-    }
-}
-
-# ------------------------------------------------------------------------------
-# linux: write configs to USER_APP_CONFIG_DIR
-#
-proc ::pd_guiprefs::write_config_x11 {data {adomain} {akey}} {
-    # right now I (yvan) assume that data are just \n separated, i.e. no keys
-    set data [join $data "\n"]
-    set filename [file join $adomain $akey]
-    if {[catch {set fl [open $filename w]} errorMsg]} {
-        ::pdwindow::error "write_config_x11 $data $akey: $errorMsg"
-    } else {
-        puts -nonewline $fl $data
-        close $fl
-    }
 }
 
 #################################################################
