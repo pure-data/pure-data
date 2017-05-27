76adf43a335c80fcb96fa6df6c32a2cf60281c4f
JP Cimalando <jpcima@users.noreply.github.com>
2017-05-27 18:45:31 +0200

    cleanup FFT memory when finished

--- src/d_fft_fftsg.c
+++ src/d_fft_fftsg.c
@@ -83,6 +83,17 @@ static int ooura_init( int n)
     return (1);
 }
 
+void ooura_term() {
+  if (!ooura_maxn)
+    return;
+  t_freebytes(ooura_bitrev, ooura_bitrevsize);
+  t_freebytes(ooura_costab, ooura_maxn * sizeof(FFTFLT)/2);
+  ooura_maxn = 0;
+  ooura_bitrev = 0;
+  ooura_bitrevsize = 0;
+  ooura_costab = 0;
+}
+
 EXTERN void mayer_fht(t_sample *fz, int n)
 {
     post("FHT: not yet implemented");
