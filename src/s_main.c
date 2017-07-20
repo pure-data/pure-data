a1de1d23c7dece23fb17c3fdec03a130568c75c1
danomatika <danomatika@gmail.com>
2017-07-20 08:54:58 +0200

    removed commented font defaults

--- src/s_main.c
+++ src/s_main.c
@@ -73,13 +73,6 @@ int sys_midioutdevlist[MAXMIDIOUTDEV] = {1};
 
 char sys_font[100] = "DejaVu Sans Mono";
 char sys_fontweight[10] = "normal";
-// #ifdef __APPLE__
-// char sys_font[100] = "Monaco";
-// char sys_fontweight[10] = "normal";
-// #else
-// char sys_font[100] = "DejaVu Sans Mono";
-// char sys_fontweight[10] = "bold";
-// #endif
 static int sys_main_srate;
 static int sys_main_advance;
 static int sys_main_callback;
