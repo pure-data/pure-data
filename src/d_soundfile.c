6d28c713a6ccafa6858252eb24c23d401a23be68
danomatika <danomatika@gmail.com>
2017-07-22 02:55:38 +0200

    set soundfiler samplerate output to system sr when reading with -raw flag

--- src/d_soundfile.c
+++ src/d_soundfile.c
@@ -1264,6 +1264,7 @@ static void soundfiler_read(t_soundfiler *x, t_symbol *s,
                 bigendian = 0;
             else
                 bigendian = garray_ambigendian();
+            samprate = sys_getsr();
             argc -= 5; argv += 5;
         }
         else if (!strcmp(flag, "resize"))
