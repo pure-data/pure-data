8cb364a1a4abd446c894b8ec4854a09db6399d95
danomatika <danomatika@gmail.com>
2017-08-30 16:47:23 +0200

    prefer more compact version of last commit

--- src/s_midi_alsa.c
+++ src/s_midi_alsa.c
@@ -134,13 +134,8 @@ void sys_alsa_putmidimess(int portno, int a, int b, int c)
     snd_seq_ev_clear(&ev);
     if (portno >= 0 && portno < alsa_nmidiout)
     {
-        if (a < MIDI_TIMECODE)
-        {
-            /* channel voice message */
-            status = a & 0xf0;
-            channel = a & 0x0f;
-        }
-        else status = a & 0xff;
+        status = (status >= MIDI_SYSEX) ? status : (status & 0xf0);
+        channel = a & 0x0f;
         switch (status)
         {
             case MIDI_NOTEON:
