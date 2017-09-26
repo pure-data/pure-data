eed18dd4b57000d0ebfe9d75b76b0c94609d45de
msp <msp@elroy.localdomain>
2017-09-25 17:11:09 -0700

    Fix bug in which stdout didn't catch on that it should send to parent pd~ in binary; this was caused by different startup order when pd~ was started with or without GUI (it failed when non-GUI, binary mode).

--- src/m_class.c
+++ src/m_class.c
@@ -914,7 +914,7 @@ void pd_typedmess(t_pd *x, t_symbol *s, int argc, t_atom *argv)
                 ap++;
                 break;
             case A_FLOAT:
-                if (!argc) goto badarg;
+                if (!argc) goto badarg;  /* falls through */
             case A_DEFFLOAT:
                 if (!argc) *dp = 0;
                 else
@@ -928,7 +928,7 @@ void pd_typedmess(t_pd *x, t_symbol *s, int argc, t_atom *argv)
                 dp++;
                 break;
             case A_SYMBOL:
-                if (!argc) goto badarg;
+                if (!argc) goto badarg;  /* falls through */
             case A_DEFSYM:
                 if (!argc) *ap = (t_int)(&s_);
                 else
