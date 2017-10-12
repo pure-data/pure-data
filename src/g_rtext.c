cb6d0c62b0f82a6feb4cd7aed519c8d7aee420d4
danomatika <danomatika@gmail.com>
2017-10-12 19:42:32 +0200

    revert pd-extended margins which messed up font positioning (unless using Monaco...)

--- src/g_rtext.c
+++ src/g_rtext.c
@@ -17,14 +17,8 @@
 
 #define LMARGIN 2
 #define RMARGIN 2
-/* for some reason, it draws text 1 pixel lower on Mac OS X */
-#ifdef __APPLE__
-#define TMARGIN 3
-#define BMARGIN 1
-#else
-#define TMARGIN 4
-#define BMARGIN 0
-#endif
+#define TMARGIN 2
+#define BMARGIN 2
 
 #define SEND_FIRST 1
 #define SEND_UPDATE 2
