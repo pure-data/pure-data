bd61f2bc600a072cbaa3b98a19b0c68b47099277
Diego Barrios Romero <eldruin@gmail.com>
2018-03-23 13:36:48 +0100

    Remove unused variables

--- src/g_clone.c
+++ src/g_clone.c
@@ -146,7 +146,7 @@ static void clone_in_vis(t_in *x, t_floatarg fn, t_floatarg vis)
 
 static void clone_out_anything(t_out *x, t_symbol *s, int argc, t_atom *argv)
 {
-    t_atom *outv, *ap;
+    t_atom *outv;
     int first =
         1 + (s != &s_list && s != &s_float && s != &s_symbol && s != &s_bang),
             outc = argc + first;
