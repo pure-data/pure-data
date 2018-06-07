5f3fb362662acc526a5639a2da44f8472ae85457
IOhannes m zmoelnig <zmoelnig@umlautS.umlaeute.mur.at>
2018-06-07 21:23:25 +0200

    removed disabled debugging printout

--- src/g_undo.c
+++ src/g_undo.c
@@ -12,10 +12,6 @@
 int we_are_undoing = 0;
 extern const char *canvas_undo_name;
 
-extern void glob_preset_node_list_seek_hub(void);
-extern void glob_preset_node_list_check_loc_and_update(void);
-extern void text_checkvalidwidth(t_glist *glist);
-
 t_undo_action *canvas_undo_init(t_canvas *x)
 {
     t_undo_action *a = (t_undo_action *)getbytes(sizeof(*a));
@@ -30,7 +26,8 @@ t_undo_action *canvas_undo_init(t_canvas *x)
         XULAST(x) = a;
         a->prev = NULL;
         a->name = "no";
-        gui_vmess("gui_undo_menu", "xss", (t_int)a->x, "no", "no");
+        if (glist_isvisible(x) && glist_istoplevel(x))
+            sys_vgui("pdtk_undomenu .x%lx no no\n", x);
     }
     else
     {
@@ -43,20 +40,20 @@ t_undo_action *canvas_undo_init(t_canvas *x)
         a->prev = XULAST(x);
         XULAST(x) = a;
     }
-    //fprintf(stderr,"canvas_undo_init\n");
     return(a);
 }
 
 t_undo_action *canvas_undo_add(t_canvas *x, int type, const char *name,
     void *data)
 {
-    //fprintf(stderr,"canvas_undo_add %d\n", type);
     t_undo_action *a = canvas_undo_init(x);
     a->type = type;
     a->data = (void *)data;
     a->name = (char *)name;
     canvas_undo_name = name;
-    gui_vmess("gui_undo_menu", "xss", x, a->name, "no");
+    if (glist_isvisible(x) && glist_istoplevel(x))
+        sys_vgui("pdtk_undomenu .x%lx %s no\n", x, a->name);
+
     return(a);
 }
 
@@ -66,7 +63,6 @@ void canvas_undo_undo(t_canvas *x)
     if (XUQUEUE(x) && XULAST(x) != XUQUEUE(x))
     {
         we_are_undoing = 1;
-        //fprintf(stderr,"canvas_undo_undo %d\n", XULAST(x)->type);
         canvas_editmode(x, 1);
         glist_noselect(x);
         canvas_undo_name = XULAST(x)->name;
@@ -93,14 +89,10 @@ void canvas_undo_undo(t_canvas *x)
         /* here we call updating of all unpaired hubs and nodes since
            their regular call will fail in case their position needed
            to be updated by undo/redo first to reflect the old one */
-        glob_preset_node_list_seek_hub();
-        glob_preset_node_list_check_loc_and_update();
         if (glist_isvisible(x) && glist_istoplevel(x))
         {
-            gui_vmess("gui_undo_menu", "xss",
-                x, undo_action, redo_action);
-            text_checkvalidwidth(x);
-            canvas_getscroll(x);
+            if (glist_isvisible(x) && glist_istoplevel(x))
+                sys_vgui("pdtk_undomenu .x%lx %s %s\n", x, undo_action, redo_action);
         }
         canvas_dirty(x, 1);
     }
@@ -114,7 +106,6 @@ void canvas_undo_redo(t_canvas *x)
     {
         we_are_undoing = 1;
         XULAST(x) = XULAST(x)->next;
-        //fprintf(stderr,"canvas_undo_redo %d\n", XULAST(x)->type);
         canvas_editmode(x, 1);
         glist_noselect(x);
         canvas_undo_name = XULAST(x)->name;
@@ -140,14 +131,10 @@ void canvas_undo_redo(t_canvas *x)
         /* here we call updating of all unpaired hubs and nodes since their
            regular call will fail in case their position needed to be updated
            by undo/redo first to reflect the old one */
-        glob_preset_node_list_seek_hub();
-        glob_preset_node_list_check_loc_and_update();
         if (glist_isvisible(x) && glist_istoplevel(x))
         {
-            gui_vmess("gui_undo_menu", "xss",
-                x, undo_action, redo_action);
-            text_checkvalidwidth(x);
-            canvas_getscroll(x);
+            if (glist_isvisible(x) && glist_istoplevel(x))
+                sys_vgui("pdtk_undomenu .x%lx %s %s\n", x, undo_action, redo_action);
         }
         canvas_dirty(x, 1);
     }
@@ -158,13 +145,11 @@ void canvas_undo_rebranch(t_canvas *x)
 {
     int dspwas = canvas_suspend_dsp();
     t_undo_action *a1, *a2;
-    //fprintf(stderr,"canvas_undo_rebranch");
     if (XULAST(x)->next)
     {
         a1 = XULAST(x)->next;
         while(a1)
         {
-            //fprintf(stderr,".");
             switch(a1->type)
             {
                 case 1:    canvas_undo_connect(x, a1->data, UNDO_FREE); break;         //connect
@@ -187,7 +172,6 @@ void canvas_undo_rebranch(t_canvas *x)
         }
     }
     canvas_resume_dsp(dspwas);
-    //fprintf(stderr,"done!\n");
 }
 
 void canvas_undo_check_canvas_pointers(t_canvas *x)
@@ -206,13 +190,11 @@ void canvas_undo_free(t_canvas *x)
 {
     int dspwas = canvas_suspend_dsp();
     t_undo_action *a1, *a2;
-    //fprintf(stderr,"canvas_undo_free");
     if (XUQUEUE(x))
     {
         a1 = XUQUEUE(x);
         while(a1)
         {
-            //fprintf(stderr,".");
             switch(a1->type)
             {
                 case 0: break;                                                        //init
@@ -236,5 +218,4 @@ void canvas_undo_free(t_canvas *x)
         }
     }
     canvas_resume_dsp(dspwas);
-    //fprintf(stderr,"done!\n");
 }
