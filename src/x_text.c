22753a44eb114fe019af0db475a56b68ce564259
Christof Ressi <christof.ressi@student.kug.ac.at>
2018-06-20 12:39:19 +0200

    only increment gl_valid when deleting scalars and [text] (because it owns a scalar and uses the parent canvas as a stub)

--- src/x_text.c
+++ src/x_text.c
@@ -384,6 +384,7 @@ static void text_define_free(t_text_define *x)
         pd_unbind(&x->x_ob.ob_pd, x->x_bindsym);
     gpointer_unset(&x->x_gp);
     pd_free(&x->x_scalar->sc_gobj.g_pd);
+    x->x_canvas->gl_valid = ++glist_valid; /* invalidate pointers */
 }
 
 /* ---  text_client - common code for objects that refer to text buffers -- */
