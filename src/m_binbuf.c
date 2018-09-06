pure-data/pure-data

commit 92b9e5bc49dfaf6ce33009b5af286b78957c89d4

got rid of compiler wornings, some (deliberate string truncations) by ignoring them; some (switch fall throughs) by declaring them in comments, and some strncat calls made more correct

src/m_binbuf.c
@@ -552,17 +552,17 @@ t_symbol *binbuf_realizedollsym(t_symbol *s, int ac, t_atom *av, int tonew)
             return 0; /* JMZ: this should mimic the original behaviour */
         }
 
-        strncat(buf2, buf, MAXPDSTRING/2-1);
+        strncat(buf2, buf, MAXPDSTRING-strlen(buf)-1);
         str+=next;
         substr=strchr(str, '$');
         if(substr)
         {
-            strncat(buf2, str, (substr-str));
+            strncat(buf2, str, MAXPDSTRING-strlen(buf)-1);
             str=substr+1;
         }
         else
         {
-            strncat(buf2, str, MAXPDSTRING-1);
+            strncat(buf2, str, MAXPDSTRING-strlen(buf)-1);
             goto done;
         }
     }

src/d_soundfile.c
@@ -839,7 +839,7 @@ static int create_soundfile(t_canvas *canvas, const char *filename,
     t_aiff *aiffhdr = (t_aiff *)headerbuf;
     int fd, headersize = 0;
 
-    strncpy(filenamebuf, filename, MAXPDSTRING-10);
+    strncpy(filenamebuf, filename, MAXPDSTRING);
     filenamebuf[MAXPDSTRING-10] = 0;
 
     if (filetype == FORMAT_NEXT)

src/g_canvas.c
@@ -612,7 +612,7 @@ void canvas_reflecttitle(t_canvas *x)
     else namebuf[0] = 0;
     if (x->gl_edit)
     {
-        strncat(namebuf, " [edit]", MAXPDSTRING);
+        strncat(namebuf, " [edit]", MAXPDSTRING-strlen(namebuf)-1);
         namebuf[MAXPDSTRING-1] = 0;
     }
     sys_vgui("pdtk_canvas_reflecttitle .x%lx {%s} {%s} {%s} %d\n",

src/g_editor.c
@@ -2656,7 +2656,7 @@ void canvas_doconnect(t_canvas *x, int xpos, int ypos, int which, int doit)
                              */
                         int mode = 0;
                         int i;
-                        unsigned int sinks = 0, sources = 0;
+                        int sinks = 0, sources = 0;
                         t_float ysinks = 0., ysources = 0.;
                         int msgout = !obj_issignaloutlet(ob1, closest1);
                         int sigin = obj_issignalinlet(ob2, closest2);

src/g_undo.c
@@ -150,7 +150,7 @@ static int canvas_undo_doit(t_canvas *x, t_undo_action *udo, int action, const c
             /* undo sequences are handled in canvas_undo_undo resp canvas_undo_redo */
     case UNDO_SEQUENCE_START: return 1;                                            //start undo sequence
     case UNDO_SEQUENCE_END: return 1;                                              //end undo sequence
-    case UNDO_INIT:         if (UNDO_FREE == action) return 1;                     //init
+    case UNDO_INIT:         if (UNDO_FREE == action) return 1;/* FALLS THROUGH */  //init
     default:
         error("%s: unsupported undo command %d", funname, udo->type);
     }

src/makefile.gnu
@@ -58,7 +58,7 @@ CPPFLAGS = -DPD -DHAVE_LIBDL -DHAVE_UNISTD_H -DHAVE_ALLOCA_H \
     -D_LARGEFILE64_SOURCE -DINSTALL_PREFIX=\"$(prefix)\" \
     -Wall -W -Wstrict-prototypes  -Wno-address\
     -Wno-unused -Wno-unused-parameter -Wno-parentheses -Wno-switch \
-    -Wno-cast-function-type
+    -Wno-cast-function-type -Wno-stringop-truncation -Wno-format-truncation
 
 # code generation flags (e.g., optimization).  
 CODECFLAGS = -g -O3 -ffast-math -funroll-loops -fomit-frame-pointer

src/s_utf8.c
@@ -80,11 +80,10 @@ int u8_utf8toucs2(uint16_t *dest, int sz, char *src, int srcsz)
         }
         ch = 0;
         switch (nb) {
-            /* these fall through deliberately */
-        case 3: ch += (unsigned char)*src++; ch <<= 6;
-        case 2: ch += (unsigned char)*src++; ch <<= 6;
-        case 1: ch += (unsigned char)*src++; ch <<= 6;
-        case 0: ch += (unsigned char)*src++;
+        case 3: ch += (unsigned char)*src++; ch <<= 6; /* falls through */
+        case 2: ch += (unsigned char)*src++; ch <<= 6; /* falls through */
+        case 1: ch += (unsigned char)*src++; ch <<= 6; /* falls through */
+        case 0: ch += (unsigned char)*src++; /* falls through */
         }
         ch -= offsetsFromUTF8[nb];
         dest[i++] = ch;

src/x_vexp_if.c
@@ -362,6 +362,7 @@ SDY the following coredumps why?
                                 break;
                         } else
                                 post("expr: internal error expr_new");
+                                /* falls through */
                 default:
                         pd_error(x, "expr: bad type (%lx) inlet = %d\n",
                                             eptr->ex_type, i + 1);

commit 93d32f3a1a09053ba2cee3538063e0d657e26ad9

fix dollarsym regression
- restore dollar separated substrings parsing
 (was broken by commit 92b9e5bc49dfaf6ce33009b5af286b78957c89d4)
- fix MAXPDSTRING safeguards

src/m_binbuf.c
@@ -567,17 +567,19 @@ t_symbol *binbuf_realizedollsym(t_symbol *s, int ac, t_atom *av, int tonew)
             return 0; /* JMZ: this should mimic the original behaviour */
         }
 
-        strncat(buf2, buf, MAXPDSTRING-strlen(buf)-1);
+        strncat(buf2, buf, MAXPDSTRING-strlen(buf2)-1);
         str+=next;
         substr=strchr(str, '$');
         if(substr)
         {
-            strncat(buf2, str, MAXPDSTRING-strlen(buf)-1);
+            int n = substr-str;
+            if(n>MAXPDSTRING-strlen(buf2)-1) n=MAXPDSTRING-strlen(buf2)-1;
+            strncat(buf2, str, n);
             str=substr+1;
         }
         else
         {
-            strncat(buf2, str, MAXPDSTRING-strlen(buf)-1);
+            strncat(buf2, str, MAXPDSTRING-strlen(buf2)-1);
             goto done;
         }
     }