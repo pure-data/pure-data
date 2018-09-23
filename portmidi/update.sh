1899839f17572f8399574f8205c50e72957b0ada
danomatika <danomatika@gmail.com>
2018-09-24 00:05:50 +0200

    portaudio & portmidi script formatting and print updates

--- portmidi/update.sh
+++ portmidi/update.sh
@@ -1,4 +1,4 @@
-##! /bin/bash
+#! /bin/bash
 #
 # this script automatically downloads and copies the portmidi library for Pd
 #
@@ -25,39 +25,43 @@ VERSION=""
 
 # copy .h & .c files from $SRC to $DEST, ignore missing file errors
 function copysrc {
-	mkdir -p $DEST/$1
-	cp -v $SRC/$1/*.h $DEST/$1 2>/dev/null || :
-	cp -v $SRC/$1/*.c $DEST/$1 2>/dev/null || :
+    mkdir -p $DEST/$1
+    cp -v $SRC/$1/*.h $DEST/$1 2>/dev/null || :
+    cp -v $SRC/$1/*.c $DEST/$1 2>/dev/null || :
 }
 
 ##### GO
 
 # append revision number to checkout rule ala @###
 if [ $# -gt 0 ] ; then
-	VERSION="@$1"
+    VERSION="@$1"
 fi
 
 # move to this scripts dir
 cd $(dirname $0)
 
 # checkout source
+echo "==== Downloading portmidi $VERSION"
 if [ -d $SRC ] ; then
-	rm -rf $SRC
+    rm -rf $SRC
 fi
 svn checkout https://svn.code.sf.net/p/portmedia/code/portmidi/trunk${VERSION} $SRC
 
 # apply patches, note: this probably can't handle filenames with spaces
 # temp disable exit on error since the exit value of patch --dry-run is used
-set +e
+echo "==== Applying any patches"
 for p in $(find ./patches -type f -name "*.patch") ; do
     cd $SRC
+    set +e
     (patch -p0 -N --silent --dry-run --input "../${p}" > /dev/null 2>&1)
+    set -e
     if [[ $? == 0 ]] ; then
         patch -p0 < "../${p}"
     fi
     cd ../
 done
-set -e
+
+echo "==== Copying"
 
 # copy what we need, namely the main headers and relevant sources
 copysrc pm_common
