d50312a0392f64ef04e0529eee3f4b955f81018b
Christof Ressi <christof.ressi@gmx.at>
2019-04-26 15:49:21 +0200

    call canvas_closebang in "menuclose" message
    when a root canvas is closed, we want to send a closebang to all children, in the same order as loadbang.
    this also fixes a bug where [iemguts/closebang] wouldn't work on the toplevel root canvas.

--- src/g_editor.c
+++ src/g_editor.c
@@ -3315,6 +3315,12 @@ void glob_verifyquit(void *dummy, t_floatarg f)
     else glob_quit(0);
 }
 
+static void canvas_doclose(t_canvas *x)
+{
+    canvas_closebang(x);
+    pd_free(&x->gl_pd);
+}
+
     /* close a window (or possibly quit Pd), checking for dirty flags.
        The "force" parameter is interpreted as follows:
        0 - request from GUI to close, verifying whether clean or dirty
@@ -3357,10 +3363,10 @@ void canvas_menuclose(t_canvas *x, t_floatarg fforce)
                 gensym(buf), 2, backmsg,
                 "yes");
         }
-        else pd_free(&x->gl_pd);
+        else canvas_doclose(x);
     }
     else if (force == 1)
-        pd_free(&x->gl_pd);
+        canvas_doclose(x);
     else if (force == 2)
     {
         canvas_dirty(x, 0);
@@ -3377,7 +3383,7 @@ void canvas_menuclose(t_canvas *x, t_floatarg fforce)
                         canvas_getrootfor(g), gensym(buf), 2, backmsg);
             return;
         }
-        else pd_free(&x->gl_pd);
+        else canvas_doclose(x);
     }
     else if (force == 3)
     {
