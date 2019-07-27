33230b10b3b2e5c440fbb35132d7c8b4b248b4a9
porres <porres@gmail.com>
2019-07-26 23:31:28 -0300

    getting rid of single/double change methods inhradio/vradio
    this is a message that actually takes effect for the old/deprecarted objetcs (hdl/vdl) but not the new ones, so it should just get removed from the code and documentation.
    
    closes https://github.com/pure-data/pure-data/issues/702

--- src/g_vdial.c
+++ src/g_vdial.c
@@ -703,10 +703,6 @@ void g_vradio_setup(void)
         gensym("init"), A_FLOAT, 0);
     class_addmethod(vradio_class, (t_method)vradio_number,
         gensym("number"), A_FLOAT, 0);
-    class_addmethod(vradio_class, (t_method)vradio_single_change,
-        gensym("single_change"), 0);
-    class_addmethod(vradio_class, (t_method)vradio_double_change,
-        gensym("double_change"), 0);
     class_addmethod(vradio_class, (t_method)iemgui_zoom,
         gensym("zoom"), A_CANT, 0);
     vradio_widgetbehavior.w_getrectfn = vradio_getrect;
