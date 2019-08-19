d883936354056ff89a2f8391364fbe9c962e9c41
Miller Puckette <msp@ucsd.edu>
2019-08-19 09:11:47 -0700

    bug fixes adapted from PR 733 (ypg, "zoom bugfixes").  There turns out to be deep confusion in the Pd code as to when to use glist_getzoom() and when to use gl_zoom directly; I now think almost every reference to gl_zoom should go via getzoom() but this is about 70 edits and I'm scared to make them all just before a release.

--- src/g_template.c
+++ src/g_template.c
@@ -2033,7 +2033,7 @@ static void array_motion(void *z, t_floatarg dx, t_floatarg dy)
                     TEMPLATE->array_motion_template, thisword, 1) : 0);
             fielddesc_setcoord(TEMPLATE->array_motion_xfield,
                 TEMPLATE->array_motion_template, thisword,
-                    xwas + dx * TEMPLATE->array_motion_yperpix, 1);
+                    xwas + dx * TEMPLATE->array_motion_xperpix, 1);
             if (TEMPLATE->array_motion_yfield)
             {
                 if (TEMPLATE->array_motion_fatten)
@@ -2620,7 +2620,7 @@ static void drawnumber_vis(t_gobj *z, t_glist *glist,
         sys_vgui(".x%lx.c create text %d %d -anchor nw -fill %s -text {%s}",
                 glist_getcanvas(glist), xloc, yloc, colorstring, buf);
         sys_vgui(" -font {{%s} -%d %s}", sys_font,
-            sys_hostfontsize(glist_getfont(glist), 1),
+            sys_hostfontsize(glist_getfont(glist), glist_getzoom(glist)),
                 sys_fontweight);
         sys_vgui(" -tags [list drawnumber%lx label]\n", data);
     }
