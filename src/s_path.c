25370a4c685f7d69d212aed0459e75d731804d42
IOhannes m zmölnig <zmoelnig@iem.at>
2019-09-09 13:18:23 +0200

    use snprintf() when creating startup/path dialog commands.

--- src/s_path.c
+++ src/s_path.c
@@ -47,6 +47,9 @@
 # define stat  stat64
 #endif
 
+#ifdef _MSC_VER
+# define snprintf _snprintf
+#endif
 
     /* change '/' characters to the system's native file separator */
 void sys_bashfilename(const char *from, char *to)
@@ -649,7 +652,7 @@ void glob_start_path_dialog(t_pd *dummy)
      char buf[MAXPDSTRING];
 
     sys_set_searchpath();
-    sprintf(buf, "pdtk_path_dialog %%s %d %d\n", sys_usestdpath, sys_verbose);
+    snprintf(buf, MAXPDSTRING-1, "pdtk_path_dialog %%s %d %d\n", sys_usestdpath, sys_verbose);
     gfxstub_new(&glob_pdobject, (void *)glob_start_path_dialog, buf);
 }
 
@@ -703,7 +706,7 @@ void glob_start_startup_dialog(t_pd *dummy)
     char buf[MAXPDSTRING];
 
     sys_set_startup();
-    sprintf(buf, "pdtk_startup_dialog %%s %d \"%s\"\n", sys_defeatrt,
+    snprintf(buf, MAXPDSTRING-1, "pdtk_startup_dialog %%s %d \"%s\"\n", sys_defeatrt,
         (sys_flags? sys_flags->s_name : ""));
     gfxstub_new(&glob_pdobject, (void *)glob_start_startup_dialog, buf);
 }
