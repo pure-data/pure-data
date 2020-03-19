9f0e30b1aa056b91bf1a1fa8ee9a28aeb4476a2d
Christof Ressi <christof.ressi@gmx.at>
2020-03-19 17:42:15 +0100

    allow to receive large UDP packets
    * add (UDP) receivebuffer to _instanceiter
    * add NET_MAXPACKETSIZE constant (can be overriden at compile time)
    * socketreceiver: don't allocate buffer for UDP mode, instead just use the per-instance receive buffer

--- src/u_pdsend.c
+++ src/u_pdsend.c
@@ -18,7 +18,7 @@ static void sockerror(char *s);
 /* print addrinfo lists for debugging */
 /* #define PRINT_ADDRINFO */
 
-#define BUFSIZE 4096
+char sendbuf[NET_MAXPACKETSIZE];
 
 int main(int argc, char **argv)
 {
@@ -113,12 +113,12 @@ int main(int argc, char **argv)
     /* now loop reading stdin and sending it to socket */
     while (1)
     {
-        char buf[BUFSIZE], *bp;
+        char *bp;
         int nsent, nsend;
-        if (!fgets(buf, BUFSIZE, stdin))
+        if (!fgets(sendbuf, NET_MAXPACKETSIZE, stdin))
             break;
-        nsend = strlen(buf);
-        for (bp = buf, nsent = 0; nsent < nsend;)
+        nsend = strlen(sendbuf);
+        for (bp = sendbuf, nsent = 0; nsent < nsend;)
         {
             int res = 0;
             if (protocol == SOCK_DGRAM)
