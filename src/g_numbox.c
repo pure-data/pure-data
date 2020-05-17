8aefaa4e59edfb481d930658234632edefe18efd
porres <porres@gmail.com>
2020-05-17 17:40:38 -0300

    Keep [nbx] active after hitting enter, so it allows you to type another number
    along with the previous 2 commits, this fully closes https://github.com/pure-data/pure-data/issues/851
    
    see https://github.com/pure-data/pure-data/issues/851#issuecomment-629194457

--- src/g_numbox.c
+++ src/g_numbox.c
@@ -730,7 +730,6 @@ static void my_numbox_key(void *z, t_floatarg fkey)
     {
         x->x_val = atof(x->x_buf);
         x->x_buf[0] = 0;
-        x->x_gui.x_fsf.x_change = 0;
         my_numbox_clip(x);
         my_numbox_bang(x);
         sys_queuegui(x, x->x_gui.x_glist, my_numbox_draw_update);
