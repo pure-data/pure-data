b1f48aa42013029469098fba118d38a3cecc5b4b
porres <porres@gmail.com>
2020-05-17 17:38:04 -0300

    remove active time limit for [nbx]
    When you click on an atom box, there's no time limit for you to type a value, but [nbx] has a 3 sec limit. This removes the limit.
    
    This partially attends one of the issues discussed in https://github.com/pure-data/pure-data/issues/851 - more specifically in https://github.com/pure-data/pure-data/issues/851#issuecomment-629194457

--- src/g_numbox.c
+++ src/g_numbox.c
@@ -35,15 +35,6 @@ static t_class *my_numbox_class;
 
 /* widget helper functions */
 
-static void my_numbox_tick_reset(t_my_numbox *x)
-{
-    if(x->x_gui.x_fsf.x_change && x->x_gui.x_glist)
-    {
-        x->x_gui.x_fsf.x_change = 0;
-        sys_queuegui(x, x->x_gui.x_glist, my_numbox_draw_update);
-    }
-}
-
 static void my_numbox_tick_wait(t_my_numbox *x)
 {
     sys_queuegui(x, x->x_gui.x_glist, my_numbox_draw_update);
@@ -335,7 +326,6 @@ static void my_numbox_draw_select(t_my_numbox *x, t_glist *glist)
         if(x->x_gui.x_fsf.x_change)
         {
             x->x_gui.x_fsf.x_change = 0;
-            clock_unset(x->x_clock_reset);
             x->x_buf[0] = 0;
             sys_queuegui(x, x->x_gui.x_glist, my_numbox_draw_update);
         }
@@ -402,7 +392,6 @@ static void my_numbox_save(t_gobj *z, t_binbuf *b)
     if(x->x_gui.x_fsf.x_change)
     {
         x->x_gui.x_fsf.x_change = 0;
-        clock_unset(x->x_clock_reset);
         sys_queuegui(x, x->x_gui.x_glist, my_numbox_draw_update);
     }
     binbuf_addv(b, "ssiisiiffiisssiiiisssfi", gensym("#X"), gensym("obj"),
@@ -466,7 +455,6 @@ static void my_numbox_properties(t_gobj *z, t_glist *owner)
     if(x->x_gui.x_fsf.x_change)
     {
         x->x_gui.x_fsf.x_change = 0;
-        clock_unset(x->x_clock_reset);
         sys_queuegui(x, x->x_gui.x_glist, my_numbox_draw_update);
     }
     sprintf(buf, "pdtk_iemgui_dialog %%s |nbx| \
@@ -544,7 +532,6 @@ static void my_numbox_motion(t_my_numbox *x, t_floatarg dx, t_floatarg dy)
     my_numbox_clip(x);
     sys_queuegui(x, x->x_gui.x_glist, my_numbox_draw_update);
     my_numbox_bang(x);
-    clock_unset(x->x_clock_reset);
 }
 
 static void my_numbox_click(t_my_numbox *x, t_floatarg xpos, t_floatarg ypos,
@@ -571,14 +558,12 @@ static int my_numbox_newclick(t_gobj *z, struct _glist *glist,
         {
             clock_delay(x->x_clock_wait, 50);
             x->x_gui.x_fsf.x_change = 1;
-            clock_delay(x->x_clock_reset, 3000);
 
             x->x_buf[0] = 0;
         }
         else
         {
             x->x_gui.x_fsf.x_change = 0;
-            clock_unset(x->x_clock_reset);
             x->x_buf[0] = 0;
             sys_queuegui(x, x->x_gui.x_glist, my_numbox_draw_update);
         }
@@ -719,7 +704,6 @@ static void my_numbox_key(void *z, t_floatarg fkey)
     if(c == 0)
     {
         x->x_gui.x_fsf.x_change = 0;
-        clock_unset(x->x_clock_reset);
         sys_queuegui(x, x->x_gui.x_glist, my_numbox_draw_update);
         return;
     }
@@ -747,12 +731,10 @@ static void my_numbox_key(void *z, t_floatarg fkey)
         x->x_val = atof(x->x_buf);
         x->x_buf[0] = 0;
         x->x_gui.x_fsf.x_change = 0;
-        clock_unset(x->x_clock_reset);
         my_numbox_clip(x);
         my_numbox_bang(x);
         sys_queuegui(x, x->x_gui.x_glist, my_numbox_draw_update);
     }
-    clock_delay(x->x_clock_reset, 3000);
 }
 
 static void my_numbox_list(t_my_numbox *x, t_symbol *s, int ac, t_atom *av)
@@ -845,7 +827,6 @@ static void *my_numbox_new(t_symbol *s, int argc, t_atom *argv)
     x->x_buf[0] = 0;
     my_numbox_check_minmax(x, min, max);
     iemgui_verify_snd_ne_rcv(&x->x_gui);
-    x->x_clock_reset = clock_new(x, (t_method)my_numbox_tick_reset);
     x->x_clock_wait = clock_new(x, (t_method)my_numbox_tick_wait);
     x->x_gui.x_fsf.x_change = 0;
     iemgui_newzoom(&x->x_gui);
@@ -858,7 +839,6 @@ static void my_numbox_free(t_my_numbox *x)
 {
     if(x->x_gui.x_fsf.x_rcv_able)
         pd_unbind(&x->x_gui.x_obj.ob_pd, x->x_gui.x_rcv);
-    clock_free(x->x_clock_reset);
     clock_free(x->x_clock_wait);
     gfxstub_deleteforkey(x);
 }
