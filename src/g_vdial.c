a28e85c1dd826dc7a43ccb9492fbd1f22dc8f7a2
porres <porres@gmail.com>
2020-05-21 10:48:49 -0300

    draw vradio's outlet correctly
    closes https://github.com/pure-data/pure-data/issues/1021

--- src/g_vdial.c
+++ src/g_vdial.c
@@ -171,8 +171,8 @@ void vradio_draw_io(t_vradio* x, t_glist* glist, int old_snd_rcv_flags)
     {
         sys_vgui(".x%lx.c create rectangle %d %d %d %d -fill black -tags %lxOUT%d\n",
                  canvas,
-                 xpos, ypos + x->x_gui.x_h + IEMGUI_ZOOM(x) - ioh,
-                 xpos + iow, ypos + x->x_gui.x_h,
+                 xpos, ypos + (x->x_gui.x_h * x->x_number) + IEMGUI_ZOOM(x) - ioh,
+                 xpos + iow, ypos + (x->x_gui.x_h * x->x_number),
                  x, 0);
         /* keep these above outlet */
         if(x->x_on == 0) {
