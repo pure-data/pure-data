71c6ba611fdf3bde38ea4e11b9b9601f530473f0
mcc <andi.m.mcclure@gmail.com>
2020-07-27 19:05:37 -0400

    Fix Windows build

--- src/m_pd.h
+++ src/m_pd.h
@@ -71,6 +71,7 @@ typedef unsigned __int8   uint8_t;
 typedef unsigned __int16  uint16_t;
 typedef unsigned __int32  uint32_t;
 typedef unsigned __int64  uint64_t;
+typedef unsigned __int64  ssize_t;
 typedef unsigned __int8   bool;
 #define false 0
 #define true 1
