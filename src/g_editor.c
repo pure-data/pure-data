5563051ce0183ff05c1d0f0010c6db072e9fe97a
msp <msp@ucsd.edu>
2020-08-08 14:35:09 -0700

    bog fix: lines got drawn on parent canvas when restoring object in closed GOP

--- src/g_editor.c
+++ src/g_editor.c
@@ -4339,7 +4339,7 @@ void canvas_connect(t_canvas *x, t_floatarg fwhoout, t_floatarg foutno,
             inlet_new(objsink, &objsink->ob_pd, 0, 0);
 
     if (!(oc = obj_connect(objsrc, outno, objsink, inno))) goto bad;
-    if (glist_isvisible(x))
+    if (glist_isvisible(x) && x->gl_havewindow)
     {
         sys_vgui(
             ".x%lx.c create line %d %d %d %d -width %d -tags [list l%lx cord]\n",
