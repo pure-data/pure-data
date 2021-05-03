44aec43a649ac2d529f77286491a2174977608af
IOhannes m zmölnig <zmoelnig@iem.at>
2021-05-03 17:33:14 +0200

    nicer warning about duplicate connections

--- src/g_editor.c
+++ src/g_editor.c
@@ -4341,7 +4341,7 @@ void canvas_connect(t_canvas *x, t_floatarg fwhoout, t_floatarg foutno,
 
         /* check if objects are already connected */
     if (canvas_isconnected(x, objsrc, outno, objsink, inno)) {
-        logpost(src, 3, "refusing duplicate connection");
+        logpost(src, 3, "this pair of iolets is already connected");
         goto bad;
     }
 
