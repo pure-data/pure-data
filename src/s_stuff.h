bc37e7cb4942e7c711dc609f1356d8c4c2d97f52
Christof Ressi <info@christofressi.com>
2021-11-13 01:48:43 +0100

    revert default samplerate to 44100

--- src/s_stuff.h
+++ src/s_stuff.h
@@ -133,7 +133,7 @@ typedef struct _audiosettings
 
 #define DEFMIDIDEV 0
 
-#define DEFAULTSRATE 48000
+#define DEFAULTSRATE 44100
 #ifdef _WIN32
 #define DEFAULTADVANCE 80
 #define MMIODEFBLOCKSIZE 512
