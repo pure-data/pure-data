dfc3c9ff240aa7c3b74096c10e00299a1edc7e59
Miller Puckette <msp@ucsd.edu>
2021-11-27 16:24:03 -0800

    slight fix to atan2 fix (didn't compile on traditional c compilers because of variable declaration in mid-code)

--- src/x_arithmetic.c
+++ src/x_arithmetic.c
@@ -606,15 +606,14 @@ static void *atan2_new(void)
 
 static void atan2_bang(t_atan2 *x)
 {
-    t_float r = (x->x_f1 == 0 && x->x_f2 == 0 ? 0 : ATAN2(x->x_f1, x->x_f2));
-    outlet_float(x->x_ob.ob_outlet, r);
+    outlet_float(x->x_ob.ob_outlet,
+        (x->x_f1 == 0 && x->x_f2 == 0 ? 0 : ATAN2(x->x_f1, x->x_f2)));
 }
 
 static void atan2_float(t_atan2 *x, t_float f)
 {
     x->x_f1 = f;
-    t_float r = (x->x_f1 == 0 && x->x_f2 == 0 ? 0 : ATAN2(x->x_f1, x->x_f2));
-    outlet_float(x->x_ob.ob_outlet, r);
+    atan2_bang(x);
 }
 
 static t_class *sqrt_class;     /* ----------- sqrt --------------- */
