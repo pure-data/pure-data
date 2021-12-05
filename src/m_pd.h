a995d2a80e351207baf53ad87028131661a60fa4
Miller Puckette <msp@ucsd.edu>
2021-12-05 09:01:34 -0800

    version number to 0.52-0test3

--- src/m_pd.h
+++ src/m_pd.h
@@ -11,7 +11,7 @@ extern "C" {
 #define PD_MAJOR_VERSION 0
 #define PD_MINOR_VERSION 52
 #define PD_BUGFIX_VERSION 0
-#define PD_TEST_VERSION "test2"
+#define PD_TEST_VERSION "test3"
 extern int pd_compatibilitylevel;   /* e.g., 43 for pd 0.43 compatibility */
 
 /* old name for "MSW" flag -- we have to take it for the sake of many old
