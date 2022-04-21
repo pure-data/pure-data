edaaf6312818856201247e840c92c22317973553
IOhannes m zmoelnig <zmoelnig@umlautS.umlaeute.mur.at>
2022-04-21 23:36:11 +0200

    iemguis: re-use IHEIGHT
    Closes: https://github.com/pure-data/pure-data/issues/920

--- src/g_all_guis.h
+++ src/g_all_guis.h
@@ -5,6 +5,8 @@
 
 #ifndef __g_all_guis_h_
 
+#include "g_canvas.h"
+
 #define IEM_GUI_COLNR_WHITE          0
 #define IEM_GUI_COLNR_ML_GREY        1
 #define IEM_GUI_COLNR_D_GREY         2
@@ -85,7 +87,7 @@
 #define IEM_GUI_DRAW_MODE_CONFIG 5
 #define IEM_GUI_DRAW_MODE_IO     6
 
-#define IEM_GUI_IOHEIGHT 2
+#define IEM_GUI_IOHEIGHT IHEIGHT
 
 #define IS_A_POINTER(atom,index) ((atom+index)->a_type == A_POINTER)
 #define IS_A_FLOAT(atom,index) ((atom+index)->a_type == A_FLOAT)
