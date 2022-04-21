52445c2b7b2add40f02d6a52f2ff1e0103534edf
IOhannes m zmölnig <zmoelnig@iem.at>
2022-04-21 08:43:01 +0200

    modernize [bng]

--- src/g_bang.c
+++ src/g_bang.c
@@ -56,56 +56,44 @@ static void bng_draw_io(t_bng* x, t_glist* glist, int old_snd_rcv_flags)
 static void bng_draw_config(t_bng* x, t_glist* glist)
 {
     t_canvas *canvas = glist_getcanvas(glist);
-
-    sys_vgui(".x%lx.c itemconfigure %lxLABEL -font {{%s} -%d %s} -fill #%6.6x -text {%s} \n",
-             canvas, x, x->x_gui.x_font, x->x_gui.x_fontsize * IEMGUI_ZOOM(x), sys_fontweight,
-             (x->x_gui.x_fsf.x_selected ? IEM_GUI_COLOR_SELECTED : x->x_gui.x_lcol),
-             (strcmp(x->x_gui.x_lab->s_name, "empty") ? x->x_gui.x_lab->s_name : ""));
-    sys_vgui(".x%lx.c itemconfigure %lxBASE -fill #%6.6x\n", canvas, x, x->x_gui.x_bcol);
-    sys_vgui(".x%lx.c itemconfigure %lxBUT -fill #%6.6x\n", canvas, x,
-             (x->x_flashed ? x->x_gui.x_fcol : x->x_gui.x_bcol));
-}
-
-static void bng_draw_new(t_bng *x, t_glist *glist)
-{
     int xpos = text_xpix(&x->x_gui.x_obj, glist);
     int ypos = text_ypix(&x->x_gui.x_obj, glist);
     int iow = IOWIDTH * IEMGUI_ZOOM(x), ioh = IEM_GUI_IOHEIGHT * IEMGUI_ZOOM(x);
     int inset = IEMGUI_ZOOM(x);
+
+    sys_vgui(".x%lx.c coords %lxBASE %d %d %d %d\n", canvas, x,
+             xpos, ypos, xpos + x->x_gui.x_w, ypos + x->x_gui.x_h);
+    sys_vgui(".x%lx.c itemconfigure %lxBASE -width %d -fill #%6.6x\n", canvas, x,
+             IEMGUI_ZOOM(x), x->x_gui.x_bcol);
+
+    sys_vgui(".x%lx.c coords %lxBUT %d %d %d %d\n", canvas, x,
+        xpos + inset, ypos + inset,
+        xpos + x->x_gui.x_w - inset, ypos + x->x_gui.x_h - inset);
+    sys_vgui(".x%lx.c itemconfigure %lxBUT -width %d -fill #%6.6x\n", canvas, x,
+        IEMGUI_ZOOM(x),
+        (x->x_flashed ? x->x_gui.x_fcol : x->x_gui.x_bcol));
+
+    sys_vgui(".x%lx.c coords %lxLABEL %d %d\n", canvas, x,
+        xpos + x->x_gui.x_ldx * IEMGUI_ZOOM(x), ypos + x->x_gui.x_ldy * IEMGUI_ZOOM(x));
+    sys_vgui(".x%lx.c itemconfigure %lxLABEL -text {%s} -anchor w -font {{%s} -%d %s} -fill #%6.6x\n", canvas, x,
+        (strcmp(x->x_gui.x_lab->s_name, "empty") ? x->x_gui.x_lab->s_name : ""),
+        x->x_gui.x_font, x->x_gui.x_fontsize * IEMGUI_ZOOM(x), sys_fontweight,
+        (x->x_gui.x_fsf.x_selected ? IEM_GUI_COLOR_SELECTED : x->x_gui.x_lcol));
+}
+
+static void bng_draw_new(t_bng *x, t_glist *glist)
+{
     t_canvas *canvas = glist_getcanvas(glist);
 
-    sys_vgui(".x%lx.c create rectangle %d %d %d %d -width %d -fill #%6.6x -tags [list %lxOBJ %lxBASE]\n",
-             canvas, xpos, ypos,
-             xpos + x->x_gui.x_w, ypos + x->x_gui.x_h,
-             IEMGUI_ZOOM(x),
-             x->x_gui.x_bcol,
-             x, x);
-    if(!x->x_gui.x_fsf.x_snd_able)
-        sys_vgui(".x%lx.c create rectangle %d %d %d %d -fill black -tags [list %lxOBJ %lxOUT%d outlet]\n",
-             canvas,
-             xpos, ypos + x->x_gui.x_h + IEMGUI_ZOOM(x) - ioh,
-             xpos + iow, ypos + x->x_gui.x_h,
-             x, x, 0);
-    if(!x->x_gui.x_fsf.x_rcv_able)
-        sys_vgui(".x%lx.c create rectangle %d %d %d %d -fill black -tags [list %lxOBJ %lxIN%d inlet]\n",
-             canvas,
-             xpos, ypos,
-             xpos + iow, ypos - IEMGUI_ZOOM(x) + ioh,
-             x, x, 0);
-    sys_vgui(".x%lx.c create oval %d %d %d %d -width %d -fill #%6.6x -tags [list %lxOBJ %lxBUT]\n",
-             canvas, xpos + inset, ypos + inset,
-             xpos + x->x_gui.x_w - inset, ypos + x->x_gui.x_h - inset,
-             IEMGUI_ZOOM(x),
-             (x->x_flashed ? x->x_gui.x_fcol : x->x_gui.x_bcol),
-             x, x);
-    sys_vgui(".x%lx.c create text %d %d -text {%s} -anchor w \
-             -font {{%s} -%d %s} -fill #%6.6x -tags [list %lxOBJ %lxLABEL label text]\n",
-             canvas, xpos + x->x_gui.x_ldx * IEMGUI_ZOOM(x),
-             ypos + x->x_gui.x_ldy * IEMGUI_ZOOM(x),
-             (strcmp(x->x_gui.x_lab->s_name, "empty") ? x->x_gui.x_lab->s_name : ""),
-             x->x_gui.x_font, x->x_gui.x_fontsize * IEMGUI_ZOOM(x), sys_fontweight,
-             x->x_gui.x_lcol,
-             x, x);
+    sys_vgui(".x%lx.c create rectangle 0 0 0 0 -tags [list %lxOBJ %lxBASE]\n",
+             canvas, x, x);
+    sys_vgui(".x%lx.c create oval 0 0 0 0 -tags [list %lxOBJ %lxBUT]\n",
+             canvas, x, x);
+    sys_vgui(".x%lx.c create text 0 0 -tags [list %lxOBJ %lxLABEL label text]\n",
+             canvas, x, x);
+
+    bng_draw_config(x, glist);
+    bng_draw_io(x, glist, 0);
 }
 
 static void bng_draw_erase(t_bng* x, t_glist* glist)
