642fc784904280f74cdc6e7c3b81124e87a7aad5
IOhannes m zmölnig <zmoelnig@iem.at>
2022-06-02 10:31:19 +0200

    dialog_iemgui: vu/cnv don't have a foreground color
    with the uniform iemgui_dialog_new() function, the core sends us some
    bogus color, so unset that again on the GUI...

--- tcl/dialog_iemgui.tcl
+++ tcl/dialog_iemgui.tcl
@@ -546,10 +546,12 @@ proc ::dialog_iemgui::pdtk_iemgui_dialog {mytoplevel mainheader dim_header \
             set wdt_label [_ "Size:"]
             set num_label [_ "Num cells:"] }
         "|vu|" {
+            set ::dialog_iemgui::var_color_foreground($vid) none
             set iemgui_type [_ "VU Meter"]
             set wdt_label [_ "Width:"]
             set hgt_label [_ "Height:"] }
         "|cnv|" {
+            set ::dialog_iemgui::var_color_foreground($vid) none
             set iemgui_type [_ "Canvas"]
             set wdt_label [_ "Size:"]
             set iemgui_range_header [_ "Visible Rectangle (pix)"]
