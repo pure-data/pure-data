63ece9e28b734e73741fa4e13322ce5297f4071c
IOhannes m zmölnig <zmoelnig@iem.at>
2022-10-31 12:40:14 +0100

    simplified assignment for maximum windowsize

--- tcl/pdtk_canvas.tcl
+++ tcl/pdtk_canvas.tcl
@@ -50,8 +50,7 @@ if {[tk windowingsystem] eq "win32" || \
     # also check for Tk Cocoa backend on macOS which is only stable in 8.5.13+;
     # newer versions of Tk can handle multiple monitors so allow negative pos
     proc pdtk_canvas_wrap_window {x y w h} {
-        set width [lindex [wm maxsize .] 0]
-        set height [lindex [wm maxsize .] 1]
+        foreach {width height} [wm maxsize .] {break}
 
         if {$w > $width} {
             set w $width
