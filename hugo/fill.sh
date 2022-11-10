c9ee774d3892af31558a2e8ae8538f26fb1d828f
Lucas Cordiviola <lucarda27@hotmail.com>
2022-11-10 02:11:07 -0300

    hugo: autofill oops2

--- hugo/fill.sh
+++ hugo/fill.sh
@@ -5,3 +5,5 @@ rm ./GNUmakefile.am
 while read line; do echo $line >> GNUmakefile.am ; done < fill.pre
 
 find . -type f -not -path "./GNUmakefile*" | sort | awk '{print "    ", $1, "\\"}' >> GNUmakefile.am; echo '     $(empty)' >> GNUmakefile.am
+
+truncate -s-1 GNUmakefile.am
