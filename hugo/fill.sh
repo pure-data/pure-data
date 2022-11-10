c49cb77790a5115581dac52d164341f2b4ce69fd
Lucas Cordiviola <lucarda27@hotmail.com>
2022-11-10 07:19:08 +0000

    hugo: autofill fix6

--- hugo/fill.sh
+++ hugo/fill.sh
@@ -6,7 +6,11 @@ cat fill.pre | while read; do echo "$REPLY" >> GNUmakefile.am; done
 
 find . \
  -type f -not -path "./GNUmakefile*" \
- -type f -not -path "./Makefile" \
+ -type f -not -path "./Makefile*" \
+ -type f -not -path "./automakehugo.yaml" \
+ -type f -not -path "./hugo.stamp" \
+ -type f -not -path "./.hugo_build.lock" \
+ -type f -not -path "./.gitignore" \
  | sort | awk '{print "    ", $1, "\\"}' >> GNUmakefile.am; echo '     $(empty)' >> GNUmakefile.am
 
 truncate -s-1 GNUmakefile.am
