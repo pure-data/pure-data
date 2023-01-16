fed8a5224e3898eef690b2ef9387954e436c3157
msp <msp@ucsd.edu>
2023-01-16 15:40:17 -0800

    fixed oops in release script

--- msw/build-msw-64.sh
+++ msw/build-msw-64.sh
@@ -22,8 +22,6 @@ rm -rf pd-$pdversion
 git clone ~/pd pd-$pdversion
 cd pd-$pdversion
 
-git checkout 0.53-0
-
 #copy in the ASIO SDK and rename it ASIOSDK
 (cd asio; unzip $HOME/work/asio/ASIOSDK2.3.zip;
     rm -rf __MACOSX/; mv ASIOSDK2.3 ASIOSDK)
