9d8cd6ac63b890d0aacf8ec278c71b8df0f19db2
IOhannes m zmölnig <zmoelnig@iem.at>
2023-04-18 16:20:01 +0200

    compat: always import the compat proces into the calling namespace

--- tcl/pdtcl_compat.tcl
+++ tcl/pdtcl_compat.tcl
@@ -11,88 +11,89 @@
 ## > catch {namespace import ::pdtcl_compat::dict}
 
 package provide pdtcl_compat 0.1
-if { [catch {dict create} ] } {
 namespace eval ::pdtcl_compat {
+    namespace export dict
+}
 
-## poor man's 'dict' implementation
+## poor man's 'dict' implementation (for Tcl8.4)
 # this only provides a limited set of sub-commands
 # it is also slower than the built-in
-    namespace export dict
+proc ::pdtcl_compat::dict {command args} {
+    #puts "<dict:${command}> $args"
+    switch -- "${command}" {
+        create {
+            return {}
+        }
+        exist -
+        exists {
+            set dictionary [lindex ${args} 0]
+            set key [lindex ${args} 1]
+            foreach {k v} ${dictionary} {
+                if { $k eq ${key} } {return 1}
+            }
+            return 0
+        }
+        get {
+            set key [lindex ${args} 1]
+            foreach {k v} [lindex ${args} 0] {
+                if { $k eq ${key} } {return $v}
+            }
+            return {}
+        }
+        lappend {
+            upvar [lindex ${args} 0] dictionary
+            set key [lindex ${args} 1]
+            set value [lrange ${args} 2 end]
+            set index 0
+            foreach {k v} ${dictionary} {
+                if { $k eq ${key} } {
+                    incr index
+                    set dictionary \
+                        [lreplace ${dictionary} ${index} ${index} \
+                             [concat $v ${value}]]
+                    return ${dictionary}
 
-    proc dict {command args} {
-        #puts "<dict:${command}> $args"
-        switch -- "${command}" {
-            create {
-                return {}
-            }
-            exist -
-            exists {
-                set dictionary [lindex ${args} 0]
-                set key [lindex ${args} 1]
-                foreach {k v} ${dictionary} {
-                    if { $k eq ${key} } {return 1}
                 }
-                return 0
+                incr index 2
             }
-            get {
-                set key [lindex ${args} 1]
-                foreach {k v} [lindex ${args} 0] {
-                    if { $k eq ${key} } {return $v}
-                }
-                return {}
-            }
-            lappend {
-                upvar [lindex ${args} 0] dictionary
-                set key [lindex ${args} 1]
-                set value [lrange ${args} 2 end]
-                set index 0
-                foreach {k v} ${dictionary} {
-                    if { $k eq ${key} } {
-                        incr index
-                        set dictionary \
-                            [lreplace ${dictionary} ${index} ${index} \
-                                 [concat $v ${value}]]
-                        return ${dictionary}
+            return [lappend dictionary ${key} ${value}]
+        }
+        set {
+            upvar [lindex ${args} 0] dictionary
+            set key [lindex ${args} 1]
+            set value [lindex ${args} 2]
+            set index 0
+            foreach {k v} ${dictionary} {
+                if { $k eq ${key} } {
+                    incr index
+                    set dictionary \
+                        [lreplace ${dictionary} ${index} ${index} \
+                             [concat $v ${value}]]
+                    return ${dictionary}
 
-                    }
-                    incr index 2
                 }
-                return [lappend dictionary ${key} ${value}]
+                incr index 2
             }
-            set {
-                upvar [lindex ${args} 0] dictionary
-                set key [lindex ${args} 1]
-                set value [lindex ${args} 2]
-                set index 0
-                foreach {k v} ${dictionary} {
-                    if { $k eq ${key} } {
-                        incr index
-                        set dictionary \
-                            [lreplace ${dictionary} ${index} ${index} \
-                                 [concat $v ${value}]]
-                        return ${dictionary}
+            return [lappend dictionary ${key} ${value}]
+        }
+        unset {
+            upvar [lindex ${args} 0] dictionary
+            set key [lindex ${args} 1]
+            set index 0
+            foreach {k value} ${dictionary} {
+                if { $k eq ${key} } {
+                    set dictionary \
+                        [lreplace ${dictionary} ${index} ${index}+1]
+                    return ${dictionary}
 
-                    }
-                    incr index 2
                 }
-                return [lappend dictionary ${key} ${value}]
+                incr index 2
             }
-            unset {
-                upvar [lindex ${args} 0] dictionary
-                set key [lindex ${args} 1]
-                set index 0
-                foreach {k value} ${dictionary} {
-                    if { $k eq ${key} } {
-                        set dictionary \
-                            [lreplace ${dictionary} ${index} ${index}+1]
-                        return ${dictionary}
-
-                    }
-                    incr index 2
-                }
-                return ${dictionary}
-            }
+            return ${dictionary}
         }
     }
 }
+
+if { [catch {dict create} ] } {
+    namespace import ::pdtcl_compat::dict
 }
