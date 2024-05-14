ccffe3a9e5e9770847b78660ba29fc53ffeb23b1
danomatika <danomatika@gmail.com>
2024-05-15 00:18:28 +0200

    portmidi: update.sh now uses portmidi git repo, removed bashisms, added -f option to match portaudio/update.sh and avoid accidental updates

--- portmidi/update.sh
+++ portmidi/update.sh
@@ -1,13 +1,13 @@
-#! /bin/bash
+#! /bin/sh
 #
 # this script automatically downloads and copies the portmidi library for Pd
 #
-# uses svn trunk version by default, set checkout revision # using
+# uses git master version by default, set checkout branch/tag/commit using
 # first argument:
 #
-#    ./update.sh 239
+#    ./update.sh c0d239712d
 #
-# Dan Wilcox danomatika.com 2018
+# Dan Wilcox danomatika.com 2018,2024
 #
 
 # exit on error
@@ -15,48 +15,101 @@ set -e
 
 ##### VARIABLES
 
-SRC=portmidi-svn
+SRC=portmidi-git
 DEST=portmidi
 
-# commandline arg: version is svn revision #
+# commandline arg: version can be a tag, commit, etc
+# uses git master by default
 VERSION=""
 
 ##### FUNCTIONS
 
 # copy .h & .c files from $SRC to $DEST, ignore missing file errors
 function copysrc {
-    mkdir -p $DEST/$1
-    cp -v $SRC/$1/*.h $DEST/$1 2>/dev/null || :
-    cp -v $SRC/$1/*.c $DEST/$1 2>/dev/null || :
+    mkdir -p "${DEST}/${1}"
+    cp -v "${SRC}/${1}"/*.h "${DEST}/${1}" 2>/dev/null || :
+    cp -v "${SRC}/${1}"/*.c "${DEST}/${1}" 2>/dev/null || :
 }
+
+usage() {
+    cat <<EOF 1>&2
+Usage: $0 [OPTIONS] [<VERSION>]
+
+OPTIONS
+   -f       : force (do not abort if '${DEST}/' exists)
+              this updates existing files but keeps removed files.
+              only use this if you know what you are doing!
+
+   <VERSION>  : tag/commitish of portaudio to import
+EOF
+}
+
+#### ARGUMENTS
+
+force=no
+OPTIND=1
+while getopts ":hf" opt; do
+    case "$opt" in
+        h)
+            usage
+            exit 0
+            ;;
+        f)
+            force=yes
+            ;;
+        '?')
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift "$((OPTIND-1))" # shift off the options and optional --
+
+if [ $# -gt 0 ] ; then
+    VERSION="$1"
+fi
 
 ##### GO
 
-# append revision number to checkout rule ala @###
-if [ $# -gt 0 ] ; then
-    VERSION="@$1"
-fi
-
 # move to this scripts dir
-cd $(dirname $0)
+scriptdir=$(dirname "$0")
+cd "${scriptdir}"
 
-# checkout source
-echo "==== Downloading portmidi $VERSION"
-if [ -d $SRC ] ; then
-    rm -rf $SRC
+# check if target exists
+if [ -d "${DEST}" ]; then
+    echo "Destination '${DEST}' exists" 1>&2
+    if [ "${force}" = "yes" ]; then
+        echo "  forced to update existing files" 1>&2
+    else
+        echo "  Please remove '$(pwd)/${DEST}' before running $0" 1>&2
+        echo "  Or check the '-h' flag" 1>&2
+        exit 1
+    fi
 fi
-svn checkout https://svn.code.sf.net/p/portmedia/code/portmidi/trunk${VERSION} $SRC
+
+# clone source
+echo "==== Downloading portmidi $VERSION"
+if [ -d "${SRC}" ] ; then
+    rm -rf "${SRC}"
+fi
+git clone https://github.com/PortMidi/portmidi.git "${SRC}"
+if [ "$VERSION" != "" ] ; then
+    cd "${SRC}"
+    git checkout "${VERSION}"
+    cd -
+fi
 
 # apply patches, note: this probably can't handle filenames with spaces
 # temp disable exit on error since the exit value of patch --dry-run is used
 echo "==== Applying any patches"
 for p in $(find ./patches -type f -name "*.patch") ; do
-    cd $SRC
+    cd "${SRC}"
     set +e
-    (patch -p0 -N --silent --dry-run --input "../${p}" > /dev/null 2>&1)
+    (patch -p1 -N --silent --dry-run --input "../${p}" > /dev/null 2>&1)
     set -e
     if [[ $? == 0 ]] ; then
-        patch -p0 < "../${p}"
+        echo "==== Applying ${p}"
+        patch -p1 < "../${p}"
     fi
     cd ../
 done
@@ -69,10 +122,10 @@ copysrc pm_linux
 copysrc pm_mac
 copysrc pm_win
 copysrc porttime
-cp -v $SRC/license.txt $DEST/
+cp -v "${SRC}"/license.txt "${DEST}/"
 
 # make sure the source-code is not executable
-find "${DEST}" -type f -perm -u+x "(" -name "*.h" -or -name "*-c" ")" -exec chmod -x {} +
+#find "${DEST}" -type f -perm -u+x "(" -name "*.h" -or -name "*-c" ")" -exec chmod -x {} +
 
 # cleanup
-rm -rf $SRC
+#rm -rf $SRC
