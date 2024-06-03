f990857d4166d9ec3b3ab3232b8f340afb916aec
IOhannes m zmölnig <zmoelnig@iem.at>
2024-06-03 08:42:56 +0200

    on macOS get the bundleID (for the GUI preference domain) from the env
    this should not impose any startup penalty (unlike the last attempt that
    tried to read the Info.plist)

--- tcl/pd_guiprefs.tcl
+++ tcl/pd_guiprefs.tcl
@@ -69,12 +69,9 @@ proc ::pd_guiprefs::init {} {
         "Darwin" {
             set backend "plist"
             ## on macOS the domain should be the same as the bundle ID
-            ## (disabled for now, as it slows down startup by several seconds)
-            #catch {
-            #    set ::pd_guiprefs::domain [exec defaults read
-            #                               [file join $::sys_guidir .. .. Info]
-            #                               CFBundleIdentifier]
-            #}
+            if {[info exists ::env(__CFBundleIdentifier)] && [string trim $::env(__CFBundleIdentifier)] != {}} {
+                set ::pd_guiprefs::domain $::env(__CFBundleIdentifier)
+            }
         }
         "W32" {
             set backend "registry"
