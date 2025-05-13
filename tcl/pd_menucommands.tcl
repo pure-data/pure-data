b3c449abffb4143af81a01f1c5e160172e50159a
IOhannes m zmölnig <zmoelnig@iem.at>
2025-05-13 18:26:01 +0200

    central place to set the "application name" of Pd
    this is typically "Pd", but downstream projects might want to be able
    to change this.

--- tcl/pd_menucommands.tcl
+++ tcl/pd_menucommands.tcl
@@ -237,7 +237,7 @@ proc ::pd_menucommands::set_filenewdir {mytoplevel} {
 
 # parse the textfile for the About Pd page
 proc ::pd_menucommands::menu_aboutpd {} {
-    set versionstring "Pd $::PD_MAJOR_VERSION.$::PD_MINOR_VERSION.$::PD_BUGFIX_VERSION$::PD_TEST_VERSION"
+    set versionstring "$::PD_APPLICATION_NAME $::PD_MAJOR_VERSION.$::PD_MINOR_VERSION.$::PD_BUGFIX_VERSION$::PD_TEST_VERSION"
     set filename [file join $::sys_guidir about.txt]
     if {![file exists $filename]} {
         ::pdwindow::error [_ "ignoring '%s': doesn't exist" $filename]
