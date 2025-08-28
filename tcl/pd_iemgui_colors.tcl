ece0128696287b23e5d686b5b654f05f6dea7611
Ben Wesch <benjamin.wesch@gmail.com>
2025-08-28 15:22:05 +0200

    Move submenu to patch window's Edit menu, add to translations

--- tcl/pd_iemgui_colors.tcl
+++ tcl/pd_iemgui_colors.tcl
@@ -1,48 +1,34 @@
 # IEMGUI Colors Management Plugin for Pure Data
-# Adds a submenu to the "Tools" menu for bulk setting IEMGUI object colors of a patch window
+# Adds a submenu to the patch window's "Edit" menu for bulk setting IEMGUI object colors
+
+package provide pd_iemgui_colors 0.1
 
 namespace eval ::iemgui_colors:: {}
 
-proc ::iemgui_colors::add_colors_menu {toolsmenu} {
-    if {![winfo exists $toolsmenu]} {
+proc ::iemgui_colors::initialize {} {
+    set editmenu $::patch_menubar.edit
+    if {![winfo exists $editmenu]} {
         return
     }
-    $toolsmenu add separator
-    set colorsmenu [menu $toolsmenu.colors -tearoff 0]
-    $toolsmenu add cascade -label [_ "Set GUI object colors"] -menu $colorsmenu
-    $colorsmenu add command -label [_ "adaptive"] \
+    $editmenu add separator
+    set colorsmenu [menu $editmenu.colors -tearoff 0]
+    $editmenu add cascade -label [_ "Set GUI Object Colors"] -menu $colorsmenu
+    $colorsmenu add command -label [_ "Adaptive Colors"] \
         -command {::iemgui_colors::set_colors adaptive}
-    $colorsmenu add command -label [_ "standard"] \
+    $colorsmenu add command -label [_ "Standard Colors"] \
         -command {::iemgui_colors::set_colors standard}
 }
 
 proc ::iemgui_colors::set_colors {color_type} {
-    if {[info exists ::focused_window] && [winfo class $::focused_window] eq "PatchWindow"} {
-        raise $::focused_window
-        set color_type_label [expr {$color_type eq "adaptive" ? [_ "adaptive"] : [_ "standard"]}]
-        set result [tk_messageBox -type okcancel -icon question -default "ok" \
-            -message [_ "Set %s colors for all GUI objects in this patch?" $color_type_label] \
-            -parent $::focused_window]
-        if {$result eq "ok"} {
-            ::pdsend "$::focused_window iemgui_set_colors $color_type"
-        }
-    } else {
-        tk_messageBox -type ok -icon info \
-            -message [_ "Select a patch window first to change colors."]
-    }
-}
-
-proc ::iemgui_colors::initialize {} {
-    set toolsmenu {}
-    set pdmenu [.pdwindow cget -menu]
-    for {set m 0} {$m <= [$pdmenu index end]} {incr m} {
-        set _m [$pdmenu entrycget $m -menu]
-        switch -glob $_m {
-            *.tools { set toolsmenu $_m }
-        }
-    }
-    if { [winfo exists ${toolsmenu}] } {
-        ::iemgui_colors::add_colors_menu ${toolsmenu}
+    raise $::focused_window
+    set message [expr {$color_type eq "adaptive" ? \
+        [_ "Set adaptive colors for all GUI objects in this patch?"] : \
+        [_ "Set standard colors for all GUI objects in this patch?"]}]
+    set result [tk_messageBox -type okcancel -icon question -default "ok" \
+        -message $message \
+        -parent $::focused_window]
+    if {$result eq "ok"} {
+        ::pdsend "$::focused_window iemgui_set_colors $color_type"
     }
 }
 
