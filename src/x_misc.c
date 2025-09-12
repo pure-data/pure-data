fb038f624d04e31446b686e9d204fd7cd62fa5b4
porres <porres@gmail.com>
2025-09-11 22:09:36 -0300

    Add [closebang]
    Will update documentation accordingly if this gets merged
    
    closes https://github.com/pure-data/pure-data/issues/496#issuecomment-3282687438

--- src/x_misc.c
+++ src/x_misc.c
@@ -98,6 +98,35 @@ static void random_setup(void)
         gensym("seed"), A_FLOAT, 0);
 }
 
+/* -------------------------- closebang ------------------------------ */
+static t_class *closebang_class;
+
+typedef struct _closebang
+{
+    t_object x_obj;
+} t_closebang;
+
+static void *closebang_new(void)
+{
+    t_closebang *x = (t_closebang *)pd_new(closebang_class);
+    outlet_new(&x->x_obj, &s_bang);
+    return (x);
+}
+
+static void closebang_closebang(t_closebang *x, t_floatarg action)
+{
+    if (action == LB_CLOSE)
+        outlet_bang(x->x_obj.ob_outlet);
+}
+
+static void closebang_setup(void)
+{
+    closebang_class = class_new(gensym("closebang"), (t_newmethod)closebang_new, 0,
+        sizeof(t_closebang), CLASS_NOINLET, 0);
+    class_addmethod(closebang_class, (t_method)closebang_closebang,
+        gensym("loadbang"), A_DEFFLOAT, 0);
+    class_sethelpsymbol(closebang_class, gensym("loadbang"));
+}
 
 /* -------------------------- loadbang ------------------------------ */
 static t_class *loadbang_class;
