da27d6c006ef757ea8c42036954f3e22e6c22952
IOhannes m zmölnig <zmoelnig@iem.at>
2025-10-23 09:10:13 +0200

    Fix memleak with iemgui's private data

--- src/g_all_guis.c
+++ src/g_all_guis.c
@@ -1060,6 +1060,8 @@ void iemgui_free(t_iemgui *iemgui) {
         pd_unbind(&iemgui->x_obj.ob_pd, iemgui->x_rcv);
     pdgui_stub_deleteforkey(iemgui);
     sys_unqueuegui(iemgui);
+
+    freebytes(iemgui->x_private, sizeof(*iemgui->x_private));
 }
 
 
