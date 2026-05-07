7cbc2081790f16a824a3a68db1efd96d264f1481
IOhannes m zmölnig <zmoelnig@iem.at>
2026-05-07 17:48:55 +0200

    canvas_open() fallback to opening a file in the cwd if it can't be found otherwise
    Related: https://github.com/pure-data/pure-data/issues/2888

--- src/g_canvas.c
+++ src/g_canvas.c
@@ -1951,6 +1951,10 @@ int canvas_open(const t_canvas *x, const char *name, const char *ext,
 
     canvas_path_iterate(x, (t_canvas_path_iterator)canvas_open_iter, &co);
 
+    if (co.fd < 0) {
+        co.fd = sys_trytoopenit("", name, ext, dirresult, nameresult, size, bin, 1);
+    }
+
     return (co.fd);
 }
 
